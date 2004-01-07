a0dcd135ab68294864c89705162f357226c5a906
chrismo <chrismo@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-01-07 06:23:05 +0000

    updated iec.moz.embed.rb showing how to properly access the .document property of mozaxc embedded in IE

--- wtr/cliec/contrib/mozaxc/iec.moz.embed.rb
+++ wtr/cliec/contrib/mozaxc/iec.moz.embed.rb
@@ -4,6 +4,8 @@ require 'cl/iec'
 @iec.visible=true
 fn = File.join(Dir.pwd, 'ie.embed.html')
 @iec.navigate 'file://' + fn
+sleep 2
 @moz = @iec.document.all.browser1
-@moz_doc = @iec.document.all.browser1.document
+@moz_doc = @moz.object.document
+@moz_doc.write('<html><body>success! written via <a href="http://clabs.org/wtr">IEC</a></body></html>')
 
