11cfaab40d0fd1c32de6b3607e847338e14cccd5
chrismo <chrismo@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-01-09 00:43:06 +0000

    moved the ClIEDomViewer class below the ClIEController class

--- cliec/src/iec/cliecontroller.rb
+++ cliec/src/iec/cliecontroller.rb
@@ -1,44 +1,34 @@
 # $Id$
 =begin
-  license
-  ---------------------------------------------------------------------------
-  Copyright (c) 2001-2004, Chris Morris
-  All rights reserved.
-  
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-  
-  1. Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-  
-  2. Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution.
-  
-  3. Neither the names Chris Morris, cLabs nor the names of contributors to
-  this software may be used to endorse or promote products derived from this
-  software without specific prior written permission.
-  
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
-  IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  --------------------------------------------------------------------------
-  (based on BSD Open Source License)
-  ------------
-  CONTRIBUTORS
-  ------------
-  Jonathon Kohl
-  Brian Marick
-  Bret Pettichord
-  Paul Rogers
+--------------------------------------------------------------------------
+Copyright (c) 2001-2004, Chris Morris All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the names Chris Morris, cLabs nor the names of contributors to
+this software may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------------
 =end
 
 require 'win32ole'
@@ -50,58 +40,11 @@ module CLabs
     # VERSION auto generated section
     begin                                                            
       require 'cl/util/version'                                      
-      VERSION = CLabs::Version.new(2004, 006, 0)
+      VERSION = CLabs::Version.new(2003, 362, 0)
     rescue LoadError                                                 
-      VERSION = '2004.006.0'                                            
+      VERSION = '2003.362.0'                                            
     end                                                              
     # END_VERSION
-
-    class ClIEDomViewer
-      def initialize(aIEController)
-        @iec = aIEController
-      end
-
-      def htmlRootNode
-        ClDomNodeWrapper.new(@iec.htmlNode, 'HTML')
-      end
-
-      def outputDom(io=$stdout)
-        rootNodeWrapper = htmlRootNode
-        buildNodeWrapperTree(rootNodeWrapper)
-        dumpWrapperNode(rootNodeWrapper, "-", io)
-      end
-
-      def buildNodeWrapperTree(rootNodeWrapper)
-        p rootNodeWrapper if $debug
-        rootNodeWrapper.node.childNodes.length.times do |i|
-          buildNodeWrapperTree(rootNodeWrapper.addChild(rootNodeWrapper.node.childNodes(i)))
-        end
-      end
-
-      def dumpWrapperNode(aNode, path, io=$stdout)
-        io.puts 'nodeName: ' + path + aNode.name
-        io.puts 'nodeValue: ' + aNode.node.nodeValue if !aNode.node.nodeValue.nil?
-        # p 'outerText: ' + aNode.outerText if !aNode.outerText.nil?
-        aNode.childArray.each do | childNodeWrapper |
-          dumpWrapperNode(childNodeWrapper, path + aNode.name + "-", io)
-        end
-      end
-
-      def getNodeWrapperFromPath(path, rootNodeWrapper)
-        pathParts = path.split('-')
-        pathParts.delete_at(0) if pathParts[0] == 'HTML'
-        nodeWrapper = rootNodeWrapper
-        pathParts.each do | nodeWrapperName |
-          nodeWrapper.childArray.each do | childNodeWrapper |
-            if childNodeWrapper.name == nodeWrapperName
-              nodeWrapper = childNodeWrapper
-              break
-            end
-          end
-        end
-        return nodeWrapper
-      end
-    end
 
     class ClIEController
       attr_reader :ie, :options
@@ -258,6 +201,53 @@ module CLabs
       def dump_dom(io=$stdout)
         dv = ClIEDomViewer.new(self)
         dv.outputDom(io)
+      end
+    end
+
+    class ClIEDomViewer
+      def initialize(aIEController)
+        @iec = aIEController
+      end
+
+      def htmlRootNode
+        ClDomNodeWrapper.new(@iec.htmlNode, 'HTML')
+      end
+
+      def outputDom(io=$stdout)
+        rootNodeWrapper = htmlRootNode
+        buildNodeWrapperTree(rootNodeWrapper)
+        dumpWrapperNode(rootNodeWrapper, "-", io)
+      end
+
+      def buildNodeWrapperTree(rootNodeWrapper)
+        p rootNodeWrapper if $debug
+        rootNodeWrapper.node.childNodes.length.times do |i|
+          buildNodeWrapperTree(rootNodeWrapper.addChild(rootNodeWrapper.node.childNodes(i)))
+        end
+      end
+
+      def dumpWrapperNode(aNode, path, io=$stdout)
+        io.puts 'nodeName: ' + path + aNode.name
+        io.puts 'nodeValue: ' + aNode.node.nodeValue if !aNode.node.nodeValue.nil?
+        # p 'outerText: ' + aNode.outerText if !aNode.outerText.nil?
+        aNode.childArray.each do | childNodeWrapper |
+          dumpWrapperNode(childNodeWrapper, path + aNode.name + "-", io)
+        end
+      end
+
+      def getNodeWrapperFromPath(path, rootNodeWrapper)
+        pathParts = path.split('-')
+        pathParts.delete_at(0) if pathParts[0] == 'HTML'
+        nodeWrapper = rootNodeWrapper
+        pathParts.each do | nodeWrapperName |
+          nodeWrapper.childArray.each do | childNodeWrapper |
+            if childNodeWrapper.name == nodeWrapperName
+              nodeWrapper = childNodeWrapper
+              break
+            end
+          end
+        end
+        return nodeWrapper
       end
     end
 
