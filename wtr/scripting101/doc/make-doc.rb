602c504ded75b50d07e2e693d5ca0586e1eda4d7
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-04-05 22:30:52 +0000

    Converted the lab exercises from *.doc to *.html Wrote new documentation indexes: doc/index.html and lab/exercises.exercises.html Updated install.html Actually these 3 html files are automatically built from *.txt files using make-doc.rb

---
+++ wtr/scripting101/doc/make-doc.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'redcloth'
+
+def convert( textile_file )
+  "Convert file in textile to html."
+  html = RedCloth.new( File.read( textile_file )).to_html
+  html.gsub(/<br \/>/, '') # because i like auto-fill mode
+end
+
+(Dir["*.txt"] + Dir["../lab/exercises/*.txt"]).each do |textile_file|
+  html = convert( textile_file )
+  html_file = textile_file.sub(/\.txt$/, ".html")
+  File.open( html_file, "w" ) {| f | f.write(html)}
+  puts html_file
+end
