7122e63fa33b92c869126d8fc4e60fa7baf8253f
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-05-05 18:27:39 +0000

    Added test for show_elements courtesy of Daniel Knierim. Thanks!

---
+++ scripting101/lab/toolkit/test-show_elements.rb
@@ -0,0 +1,57 @@
+# Tests show_elements method in iec-assist.rb against Timeclock server on localhost.
+# Assumes Timeclock server is already running.
+
+require 'English'
+require '../toolkit'
+require 'test/unit'
+
+ 
+class Test_login  < Test::Unit::TestCase
+  def setup()
+    start_ie("http://localhost:8080")
+    $iec.wait
+    @read_end, @write_end = IO.pipe
+    $stdout = @write_end
+  end
+  
+  # Only one form on this page; all cases in this group should show same result
+  @@regexReport = Regexp.new("^\s*name:\s*name\s+value:\s*$")
+  def check_report()
+    @write_end.close
+    @write_end = nil
+    report = @read_end.gets
+
+    assert_not_nil(report)
+    assert_match(@@regexReport, report)
+    assert(@read_end.eof?, "Report more than one line long")
+  end
+
+  def test_show_elements_form()
+    begin
+      show_elements(forms[0])
+    rescue
+      flunk("show_elements(forms[0]) threw exception: " + $ERROR_INFO)
+    else
+      check_report()
+    end
+  end
+  
+  def xtest_show_elements_noarg()
+    begin
+      show_elements()
+    rescue
+      flunk("show_elements() threw exception: " + $ERROR_INFO)
+    else
+      check_report()
+    end
+  end
+  
+  def teardown
+    $iec.close
+    if @write_end then @write_end.close end
+    @read_end.close
+    $stdout = STDOUT
+  end
+end
+
+
