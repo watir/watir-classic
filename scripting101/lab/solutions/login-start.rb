76f232fe9c2cbd5998f05dfe6c8829043b3cb420
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-05-07 04:09:56 +0000

    Revisions to Lab 3

--- scripting101/lab/solutions/login-start.rb
+++ scripting101/lab/solutions/login-start.rb
@@ -1,17 +1,20 @@
+# Suggested solution to Lab 3, Part 1, Start The Day.
+
 require 'toolkit'
 
-# assume we have a new user. (delete user-file and restart server if necessary.)
+# make sure we have a new user
+ensure_no_user_data 'ruby'
 
 # login
-start_ie("http://localhost:8080")
+start_ie 'http://localhost:8080'
 form = forms[0]
-form.name = "ruby"
+form.name = 'ruby'
 form.submit
 wait
 
 # create a background job
-new_job = form{|f| f.action == 'job'}
-new_job.name = "background"
+new_job = form {|f| f.action == 'job'}
+new_job.name = 'background'
 new_job.submit
 wait
 
