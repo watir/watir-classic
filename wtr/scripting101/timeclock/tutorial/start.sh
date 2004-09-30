586ddea6a0f791740de62d2cc5714409eb21b7de
(no author) <(no author)@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-09-30 14:15:08 +0000

    This commit was manufactured by cvs2svn to create branch 'paul'.

--- wtr/scripting101/timeclock/tutorial/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export RUBYLIB="../fluid:../ruby-trace"
-export VW_TIMECLOCK_DATA_DIR="c:/temp/timeclock"
-cd ../bin
-ruby command-line $*
--- wtr/scripting101/toolkit.rb
+++
@@ -1,8 +0,0 @@
-$: << File.dirname(__FILE__)
-require 'toolkit/iec-assist'
-require 'toolkit/table-array'
-require 'toolkit/testhook'
-require 'toolkit/assert'
-require 'toolkit/timeclock-assist'
-
-# 
