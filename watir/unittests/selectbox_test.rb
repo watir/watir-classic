7903db0710d07b47ceadce7c450bb23eea334ad1
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-10-13 03:42:49 +0000

    test select boxes

---
+++ watir/unittests/selectbox_test.rb
@@ -0,0 +1,103 @@
+
+# tests for Buttons
+# revision: $Revision$
+
+require '../watir'
+
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
+require 'testUnitAddons'
+$myDir = File.dirname(__FILE__)
+
+$LOAD_PATH << $myDir
+
+
+
+
+class TC_Selectbox < Test::Unit::TestCase
+
+
+    def gotoPage()
+        $ie.goto("file://#{$myDir}/html/selectboxes1.html")
+    end
+
+    
+
+   
+
+    def test_textBox_Exists
+       gotoPage()
+       assert($ie.selectBox(:name, "sel1").exists?)   
+       assert_false($ie.selectBox(:name, "missing").exists?)   
+
+       assert_false($ie.selectBox(:id, "missing").exists?)   
+    end
+
+
+    def test_selectBox_enabled
+       gotoPage()
+       assert($ie.selectBox(:name, "sel1").enabled?)   
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.selectBox(:name, "NoName").enabled? }  
+    end
+
+
+    def test_selectBox_getAllContents
+       gotoPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.selectBox(:name, "NoName").getAllContents }  
+       assert_arrayEquals( ["Option 1" ,"Option 2" , "Option 3" , "Option 4"] , $ie.selectBox(:name, "sel1").getAllContents)   
+    end
+
+
+    def test_selectBox_getSelectedItems
+       gotoPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.selectBox(:name, "NoName").getSelectedItems}  
+
+       assert_arrayEquals( ["Option 3" ] , $ie.selectBox(:name, "sel1").getSelectedItems)   
+       assert_arrayEquals( ["Option 3" , "Option 6" ] , $ie.selectBox(:name, "sel2").getSelectedItems)   
+    end
+
+
+    def test_clearSelection
+
+       gotoPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.selectBox(:name, "NoName").clearSelection}  
+       $ie.selectBox( :name , "sel1").clearSelection
+
+       # the box sel1 has no ability to have a de-selected item
+       assert_arrayEquals( ["Option 3" ] , $ie.selectBox(:name, "sel1").getSelectedItems)   
+
+       $ie.selectBox( :name , "sel2").clearSelection
+       assert_arrayEquals( [ ] , $ie.selectBox(:name, "sel2").getSelectedItems)   
+
+    end
+
+
+    def test_selectBox_select
+       gotoPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.selectBox(:name, "NoName").getSelectedItems}  
+
+       assert_raises(NoValueFoundException , "NoValueFoundException was supposed to be thrown" ) {   $ie.selectBox(:name, "sel1").select("missing item") }  
+       assert_raises(NoValueFoundException , "NoValueFoundException was supposed to be thrown" ) {   $ie.selectBox(:name, "sel1").select(/missing/) }  
+
+       # the select method keeps any currently selected items - use the clear selectcion method first
+       $ie.selectBox( :name , "sel1").clearSelection
+       $ie.selectBox( :name , "sel1").select("Option 1")
+       assert_arrayEquals( ["Option 1" ] , $ie.selectBox(:name, "sel1").getSelectedItems)   
+
+       $ie.selectBox( :name , "sel1").clearSelection
+       $ie.selectBox( :name , "sel1").select(/2/)
+       assert_arrayEquals( ["Option 2" ] , $ie.selectBox(:name, "sel1").getSelectedItems)   
+
+       $ie.selectBox( :name , "sel2").clearSelection
+       $ie.selectBox( :name , "sel2").select([ /2/ , /4/ ])
+       assert_arrayEquals( ["Option 2" , "Option 4" ] , $ie.selectBox(:name, "sel2").getSelectedItems)   
+
+    end
+end
+
+$ie = IE.new
