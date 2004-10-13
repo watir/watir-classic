25099de883c28a84bf73b471929b7d1e2e3beede
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-10-13 18:33:47 +0000

    First check in of radio buttons

---
+++ watir/unittests/radios_test.rb
@@ -0,0 +1,133 @@
+
+# tests for Buttons
+# revision: $Revision$
+
+require '../watir'
+
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
+require 'testUnitAddons'
+$myDir = File.dirname(__FILE__)
+
+$LOAD_PATH << $myDir
+
+
+
+
+class TC_Radios < Test::Unit::TestCase
+
+
+    def gotoRadioPage()
+        $ie.goto("file://#{$myDir}/html/radioButtons1.html")
+    end
+
+   
+    def test_Radio_Exists
+       gotoRadioPage()
+       assert($ie.radio(:name, "box1").exists?)   
+       assert($ie.radio(:id, "box5").exists?)   
+
+       assert_false($ie.radio(:name, "missingname").exists?)   
+       assert_false($ie.radio(:id, "missingid").exists?)   
+    end
+
+    def test_Radio_Enabled
+       gotoRadioPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "noName").enabled?  }  
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:id, "noName").enabled?  }  
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "box4" , 6).enabled?  }  
+
+
+
+       assert_false($ie.radio(:name, "box2").enabled?)   
+       assert($ie.radio(:id, "box5").enabled?)   
+       assert($ie.radio(:name, "box1").enabled?)   
+
+
+    end
+
+    def test_Radio_isSet
+
+       gotoRadioPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "noName").isSet?  }  
+
+       puts "radio 1 is set : #{ $ie.radio(:name, 'box1').isSet? } "
+       assert_false($ie.radio(:name, "box1").isSet?)   
+
+
+       assert( $ie.radio(:name, "box3").isSet?)   
+       assert_false($ie.radio(:name, "box2").isSet?)   
+
+
+       assert( $ie.radio(:name, "box4" , 1 ).isSet?)   
+       assert_false($ie.radio(:name, "box4" , 2 ).isSet?)   
+
+    end
+
+    def test_radio_clear
+
+       gotoRadioPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "noName").clear  }  
+
+
+       $ie.radio(:name, "box1").clear
+       assert_false($ie.radio(:name, "box1").isSet?)   
+
+
+       assert_raises(ObjectDisabledException, "ObjectDisabledException was supposed to be thrown" ) {   $ie.radio(:name, "box2").clear  } 
+       assert_false($ie.radio(:name, "box2").isSet?)   
+
+       $ie.radio(:name, "box3").clear
+       assert_false($ie.radio(:name, "box3").isSet?)   
+
+
+       $ie.radio(:name, "box4" , 1).clear
+       assert_false($ie.radio(:name, "box4" , 1).isSet?)   
+
+
+    end
+
+    def test_radio_getSTate
+
+       gotoRadioPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "noName").getState  }  
+
+       assert_equal( RadioButton::UNCHECKED , $ie.radio(:name, "box1").getState )   
+       assert_equal( RadioButton::CHECKED , $ie.radio(:name, "box3").getState)   
+
+       # radioes that have the sme name but different values
+
+       assert_equal( RadioButton::UNCHECKED , $ie.radio(:name, "box4" , 2).getState )   
+       assert_equal( RadioButton::CHECKED , $ie.radio(:name, "box4" , 1).getState)   
+
+
+    end
+
+    def test_radio_set
+
+       gotoRadioPage()
+
+       assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.radio(:name, "noName").set  }  
+
+       $ie.radio(:name, "box1").set
+       assert($ie.radio(:name, "box1").isSet?)   
+
+       assert_raises(ObjectDisabledException, "ObjectDisabledException was supposed to be thrown" ) {   $ie.radio(:name, "box2").set  }  
+
+       $ie.radio(:name, "box3").set
+       assert($ie.radio(:name, "box3").isSet?)   
+
+       # radioes that have the sme name but different values
+       $ie.radio(:name, "box4" , 3).set
+       assert($ie.radio(:name, "box4" , 3).isSet?)   
+
+    end
+
+end
+
+$ie = IE.new
