1976ae3a85a015031e430d607ce7626b2dae3dfa
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-10-19 19:22:31 +0000

    new files for demo of testing iframes

---
+++ watir/unittests/iframe_test.rb
@@ -0,0 +1,39 @@
+
+# tests for Buttons
+# revision: $Revision$
+
+require '../watir'
+
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
+require 'testUnitAddons'
+$myDir = File.dirname(__FILE__)
+
+$LOAD_PATH << $myDir
+
+
+
+
+class TC_Buttons < Test::Unit::TestCase
+
+
+    def gotoPage()
+        $ie.goto("file://#{$myDir}/html/iframeTest.html")
+    end
+
+
+    def test_Iframe
+       gotoPage()
+
+       $ie.frame("senderFrame").textField(:name , "textToSend").set( "Hello World")
+       $ie.frame("senderFrame").button(:index, 1).click
+
+       assert( $ie.frame("receiverFrame").textField(:name , "receiverText").verify_contains("Hello World") )
+
+    end
+
+   
+end
+
+$ie = IE.new
