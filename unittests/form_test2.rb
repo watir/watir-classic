e6b75ebb683a426ef73997f529a4bb9c30770613
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-11 06:16:59 +0000

    test file for forms

---
+++ unittests/form_test2.rb
@@ -0,0 +1,48 @@
+# tests for Forms
+# revision: $Revision$
+
+require '../watir'
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+require 'testUnitAddons'
+require 'setup'
+
+class TC_Forms < Test::Unit::TestCase
+
+
+    def gotoFormsPage()
+        $ie.goto("file://#{$myDir}/html/forms2.html")
+    end
+
+
+    def test_Form_Exists
+       gotoFormsPage()
+
+       assert($ie.form(:name, "test2").exists?)   
+       assert_false($ie.form(:name, "missing").exists?)   
+
+       assert($ie.form("test2").exists?)   
+       assert_false($ie.form( "missing").exists?)   
+
+       assert($ie.form(:index,  1).exists?)   
+       assert_false($ie.form(:index, 4).exists?)   
+
+       assert($ie.form(:method, "get").exists?)   
+       assert_false($ie.form(:method, "missing").exists?)   
+
+       assert($ie.form(:action, "pass.html").exists?)   
+       assert_false($ie.form(:action, "missing").exists?)   
+
+
+
+       
+    end
+
+    def test_ButtonInForm
+       gotoFormsPage()
+
+        assert($ie.form(:name ,"test2").button(:caption , "Submit").exists?)
+    end 
+
+end
+#$ie=IE.new
