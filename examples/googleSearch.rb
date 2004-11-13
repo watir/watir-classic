f377bd230206358e19f9e54d6dc914f24447ee73
jonathankohl <jonathankohl@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-13 17:21:17 +0000

    simplified the test example

--- examples/googleSearch.rb
+++ examples/googleSearch.rb
@@ -8,25 +8,13 @@
 #   * entering text into a text field                                          
 #   * clicking a button
 #   * checking to see if a page contains text.                                 
-#   * using a test::unit "assert" ( http://www.nunit.org/assertions.html )
+# Test will search Google for the "pickaxe" Ruby book
 #
 #------------------------------------------------------------------------------------------------------------#
 
-#includes
-require '../watir.rb'   # the controller
+   #includes
+   require '../watir.rb'   # the controller
 
-#test::unit includes
-require 'test/unit' 
-require 'test/unit/ui/console/testrunner'
-require '../unittests/testUnitAddons'
-
-#code to set your current path in Windows
-$myDir = File.dirname(__FILE__)
-$LOAD_PATH << $myDir
-
-class TC_google < Test::Unit::TestCase
- 
-  def test_google
    #variables
    testSite = 'http://www.google.com'
 
@@ -41,26 +29,28 @@ class TC_google < Test::Unit::TestCase
    puts '  Action: entered ' + testSite + 'in the address bar.'
 
    puts 'Step 2: enter "pickaxe: in the search text field'
-   $ie.textField(:name, "q").set("pickaxe")  # q is the name of the search field
+   $ie.textField(:name, "q").set("pickaxe")       # q is the name of the search field
    puts '  Action: entered pickaxe in the search field'
 
    puts 'Step 3: click the "Google Search" button'
-   $ie.button(:caption, "Google Search").click
+   $ie.button(:caption, "Google Search").click   # Google Search is the caption of the button
    puts '  Action: clicked the Google Search button.'
 
    puts 'Expected Result: '
    puts ' - a google page with results should be shown. "Pragmatic Programmers LLC" should be high on the list.'
   
    puts 'Actual Result: Check that the "The Pragmatic Programmers, LLC" link actually appears on the page by using an assertion'
-   assert($ie.pageContainsText("The Pragmatic Programmers, LLC") )
-
+   a = $ie.pageContainsText("The Pragmatic Programmers, LLC") 
+   if !a 
+      puts "Test Failed! Could not find test string: The Pragmatic Programmers, LLC" 
+   else
+      puts "Test Passed. Found the test string: The Pragmatic Programmers, LLC. Actual Results match Expected Results."
+   end
+   
    puts '  '
    puts '## End of test: google search'
   
-   end
-
-end 
 
 # -end of simple google search test
-#####
+
 
