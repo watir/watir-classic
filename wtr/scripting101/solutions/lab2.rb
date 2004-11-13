6953778a1fa3ff167e0282f605dd6c910a828c09
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-13 21:14:47 +0000

    moved IEC solutions to OLD directory

--- wtr/scripting101/solutions/lab2.rb
+++ wtr/scripting101/solutions/lab2.rb
@@ -1,25 +1,21 @@
-# Suggested solution to lab 2.
-# Note: This lab uses IRB, where the 'waits' aren't necessary
+# Suggested solution to lab 2 using watir.
 
-require 'toolkit'
+require 'toolkit/watir'
 
 # 1. Login using existing account
-start_ie # may have to specify URL
-forms[0].name = "bret"
-forms[0].submit
-wait
+$ie = IE.new
+$ie.goto('http://localhost:8080')
+$ie.textField(:name, 'name').set('paul')
+$ie.form(:index, 1).submit
 
 # 2. Create a new job
-form{|f| f.action == 'job'}.name = "ruby article"
-form{|f| f.action == 'job'}.submit
-wait
+$ie.form(:action, 'job').textField(:name, 'name').set('ruby article')
+$ie.form(:action, 'job').submit
 
 # 3. Start the new job
-form{|f| f.action == 'start'}.element{|e| e.value == 'ruby article'}.click
-wait
+$ie.button(:value, 'ruby article').click
 
 # 4. Stop the day
-form{|f| f.action == 'pause_or_stop_day'}.elements('stop_day').click
-wait
+$ie.button(:name, 'stop_day').click
 
 puts "COMPLETE"
--- wtr/scripting101/solutions/suite.rb
+++ wtr/scripting101/solutions/suite.rb
@@ -1,39 +1,17 @@
-# suite.rb - run all the suggested solutions tests
+# suite.rb - run all the suggested solutions tests (watir)
 # This suite presumes timeclock http server is running on same machine.
 
 require 'test/unit'
 
 $: << File.join( File.dirname( __FILE__ ), '..' )
 require 'toolkit/iostring'
-require 'toolkit/table-array'
 require 'toolkit/testhook'
-require 'toolkit/iec-assist'
-require 'toolkit/timeclock-assist'
-
-$: << File.dirname( __FILE__ )
-
-### Utility functions & classes
-def initialize_user (user, initial_job)
-  # presumes user is not already initialized
-  start_ie
-  login_form = forms[0]
-  login_form.name = user
-  login_form.submit
-  $iec.wait
-  
-  job_form = forms[0]
-  job_form.name = initial_job
-  job_form.submit
-  $iec.wait
-  sleep 2
-  $iec.close
-end
-
-### Tests
+require 'toolkit/table-array'
+require 'toolkit/watir-assist'
 
 class Lab2 < Test::Unit::TestCase
   def setup
-    ensure_no_user_data 'bret' 
+    ensure_no_user_data 'paul' 
     # Note: test just happens to work whether this is a new or existing user. 
     # It is spec'ed to only work with an existing user...
     @mockout = IOString.new ""
@@ -42,7 +20,7 @@ class Lab2 < Test::Unit::TestCase
     $stdout = @mockout
     load 'lab2.rb'
     $stdout = STDOUT
-    assert_equal "COMPLETE\n", @mockout
+    assert_match /COMPLETE\n$/, @mockout
 
     # verify one job was created and it is no longer running.
     # (presumes ie isn't closed)
@@ -51,13 +29,13 @@ class Lab2 < Test::Unit::TestCase
   end
   def teardown
     $stdout = STDOUT
-    $iec.close if $iec
+    $ie.close if $ie
   end
 end
 
 class Lab3Part1 < Test::Unit::TestCase
   def test_login_start
-    load 'login-start.rb'
+    load 'lab3_1.rb'
 
     y = get_results_table_array
     assert_equal 2, y.length
@@ -65,106 +43,8 @@ class Lab3Part1 < Test::Unit::TestCase
     assert_equal "running", y.status(1)
   end
   def teardown
-    $iec.close if $iec
+    $ie.close if $ie
     ensure_no_user_data("ruby")
   end
 end
-
-class Lab3Part2 < Test::Unit::TestCase
-  def test_start_stop
-    load 'start-stop.rb'
-
-    y = get_results_table_array
-    assert_equal 2, y.length
-    assert_equal "foreground", y.job_name(1)
-    assert_equal "", y.status(1)
-  end
-  def teardown
-    $iec.close if $iec
-    ensure_no_user_data("ruby")
-  end
-end
-
-class Lab3Part3 < Test::Unit::TestCase
-  def test_two_jobs
-    load 'two-jobs.rb'
-
-    y = get_results_table_array
-    assert_equal 3, y.length
-    assert_equal "job2", y.job_name(1)
-    assert_equal "running", y.status(1)
-    assert_equal "job1", y.job_name(2)
-    assert_equal "paused", y.status(2)
-  end
-  def teardown
-    $iec.close if $iec
-    ensure_no_user_data("ruby")
-  end
-end
-    
-class Lab4PartX < Test::Unit::TestCase
-  def setup
-    # start with user with no time records
-    @mockout = IOString.new ""
-  end
-
-  def expect string
-    assert_equal string, @mockout.readline!.strip
-  end
-  def expect_match regexp
-    assert_match regexp, @mockout.readline!.strip
-  end
-
-  def test_check_records
-    $stdout = @mockout
-    load 'check-records.rb'
-    $stdout = STDOUT
-  end
-  def teardown
-    $stdout = STDOUT
-    $iec.close if $iec
-  end
-end
-
-class Test1 < Test::Unit::TestCase
-  def setup
-    ensure_no_user_data("STANZ")
-    initialize_user "STANZ", "background"
-    @mockout = IOString.new ""
-  end
-  def test_test1
-    $stdout = @mockout
-    load 'test1.rb'
-    sleep 2
-    $stdout = STDOUT
-    assert_no_match /FAIL/, @mockout 
-    assert @mockout !~ /FAIL/
-  end
-  def teardown
-    $stdout = STDOUT
-    $iec.close if $iec
-  end
-end
-
-class ErikTest < Test::Unit::TestCase
-  def setup
-    ensure_no_user_data("Erik")
-    @mockout = IOString.new ""
-  end
-  def test_erik
-    $stdout = @mockout
-    load 'erik.rb'
-    $stdout = STDOUT
-    assert_no_match /FAIL/, @mockout 
-    assert @mockout !~ /FAIL/
-    assert_match /PASS.*PASS/, @mockout 
-  end
-  def teardown
-    $stdout = STDOUT
-    $iec.close if $iec
-  end
-end
-
-
-
 
