2fc816063ab545426182b2cc594017b5a4c5b112
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-13 21:07:53 +0000

    updates

--- wtr/scripting101/solutions/suitew.rb
+++ wtr/scripting101/solutions/suitew.rb
@@ -1,9 +1,13 @@
+# suite.rb - run all the suggested solutions tests (watir)
+# This suite presumes timeclock http server is running on same machine.
+
 require 'test/unit'
 
 $: << File.join( File.dirname( __FILE__ ), '..' )
 require 'toolkit/iostring'
 require 'toolkit/testhook'
-
+require 'toolkit/table-array'
+require 'toolkit/watir-assist'
 
 class Lab2 < Test::Unit::TestCase
   def setup
@@ -20,11 +24,27 @@ class Lab2 < Test::Unit::TestCase
 
     # verify one job was created and it is no longer running.
     # (presumes ie isn't closed)
-#    assert_total_job_records 1
-#    assert_job_record 1, 'ruby article', ''
+    assert_total_job_records 1
+    assert_job_record 1, 'ruby article', ''
   end
   def teardown
     $stdout = STDOUT
-#    $ie.close if $ie
+    $ie.close if $ie
   end
 end
+
+class Lab3Part1 < Test::Unit::TestCase
+  def test_login_start
+    load 'lab3_1.rb'
+
+    y = get_results_table_array
+    assert_equal 2, y.length
+    assert_equal "background", y.job_name(1)
+    assert_equal "running", y.status(1)
+  end
+  def teardown
+    $ie.close if $ie
+    ensure_no_user_data("ruby")
+  end
+end
+
