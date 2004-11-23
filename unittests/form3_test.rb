1f0b663f5367204df7557363d7f4f5d8f771a50a
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-23 00:38:23 +0000

    new files to test various new functionality

---
+++ unittests/form3_test.rb
@@ -0,0 +1,90 @@
+# tests for Forms
+# revision: $Revision$
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
+require 'watir'
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+require 'testUnitAddons'
+require 'unittests/setup'
+
+class TC_Forms < Test::Unit::TestCase
+
+
+    def gotoFormsPage()
+        $ie.goto("file://#{$myDir}/html/forms3.html")
+    end
+
+
+    def test_Form_Exists
+       gotoFormsPage()
+
+       assert($ie.form(:name, "test2").exists?)   
+       assert_false($ie.form(:name, "missing").exists?)   
+
+       assert($ie.form("test2").exists?)   
+       assert_false($ie.form( "missing").exists?)   
+
+       assert($ie.form(:index,  1).exists?)   
+       assert_false($ie.form(:index, 88).exists?)   
+
+       assert($ie.form(:method, "get").exists?)   
+       assert_false($ie.form(:method, "missing").exists?)   
+
+       assert($ie.form(:action, "pass.html").exists?)   
+       assert_false($ie.form(:action, "missing").exists?)   
+
+
+
+       
+    end
+
+    def test_showforms
+       gotoFormsPage()
+       puts"--------------------------- forms----------------------"
+        $ie.showForms
+    end
+
+    def test_Flash
+       gotoFormsPage()
+
+        $ie.form(:name ,"test2").button(:caption , "Submit").flash
+    end 
+
+    def test_objects_with_same_name
+        gotoFormsPage()
+        assert_equal('textfield' ,$ie.textField( :name , 'g1').getProperty('value') )
+        assert_equal('button'    ,$ie.button(    :name , 'g1').getProperty('value') )
+        assert_equal('1'         ,$ie.checkBox(  :name , 'g1').getProperty('value') )
+        assert_equal('2'         ,$ie.radio(     :name , 'g1').getProperty('value') )
+
+        assert_equal('textfield_id' ,$ie.textField( :id , 'g1').getProperty('value') )
+        assert_equal('button_id'    ,$ie.button(    :id , 'g1').getProperty('value') )
+        assert_equal('1_id'         ,$ie.checkBox(  :id , 'g1').getProperty('value') )
+        assert_equal('2_id'         ,$ie.radio(     :id , 'g1').getProperty('value') )
+
+
+
+    end
+
+
+
+    def test_flash
+        $ie.button( 'Click Me').flash
+        assert_raises( UnknownObjectException ) { $ie.textField( :name , 'g177').flash   }
+    end
+
+    def test_showElements
+        $ie.showAllObjects
+    end
+
+    def test_showText
+       puts $ie.getText
+    end
+
+    def test_showHTML
+       puts $ie.getHTML
+    end
+
+
+end
---
+++ unittests/table_test.rb
@@ -0,0 +1,54 @@
+# tests for Forms
+# revision: $Revision$
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
+require 'watir'
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+require 'testUnitAddons'
+require 'unittests/setup'
+
+class TC_Tables < Test::Unit::TestCase
+
+
+    def gotoTablePage()
+        $ie.goto("file://#{$myDir}/html/table1.html")
+    end
+
+
+    def test_Table_Exists
+       gotoTablePage()
+       assert_false($ie.table(:id , 'missingTable').exists? )
+       assert_false($ie.table(:index, 33).exists? )
+
+       assert($ie.table(:id, 't1').exists? )
+       assert($ie.table(:index, 1).exists? )
+
+    end
+
+    def test_rows
+       assert_raises( UnknownTableException  ){ $ie.table(:id , 'missingTable').rows }
+       assert_raises( UnknownTableException  ){ $ie.table(:index , 3).rows }
+
+        assert_equal( 2 , $ie.table(:index , 1).rows)
+        assert_equal( 5 , $ie.table(:id, 't1').rows)   # 4 rows and a header 
+
+    end
+
+    def test_columns
+       gotoTablePage()
+
+       assert_raises( UnknownTableException  ){ $ie.table(:id , 'missingTable').columns }
+       assert_raises( UnknownTableException  ){ $ie.table(:index , 3).columns }
+
+
+        assert_equal( 2 , $ie.table(:index , 1).columns)
+        assert_equal( 2 , $ie.table(:id, 't1').columns)   # row one has 1 cell with a colspan of 2
+    end
+
+    def test_to_a
+
+        table1Expected = [ ["Row 1 Col1" , "Row 1 Col2"] ,[ "Row 2 Col1" , "Row 2 Col2"] ]
+        assert_arrayEquals(table1Expected, $ie.table(:index , 1).to_a )
+    end
+end
