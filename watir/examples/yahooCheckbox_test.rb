f8dcecf94b34a042a641543b5fdd233c8dfc172e
jonathankohl <jonathankohl@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-11-27 19:37:20 +0000

    minor tweaks

--- watir/examples/yahooCheckbox_test.rb
+++ watir/examples/yahooCheckbox_test.rb
@@ -13,7 +13,7 @@
 #------------------------------------------------------------------------------------------------------------ #
 
 #includes
-require '../watir.rb'   # the controller
+require '../watir'   # the controller
 
 #test::unit includes
 require 'test/unit' 
