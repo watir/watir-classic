d920b346d29d96f57b2e17e32e79a51f5e0c1b0d
jonathankohl <jonathankohl@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-12-18 23:17:59 +0000

    minor tweaks

--- watir/examples/googleSearch.rb
+++ watir/examples/googleSearch.rb
@@ -21,34 +21,34 @@
    #open the IE browser
    $ie = IE.new
 
-   puts '## Beginning of test: google search'
-   puts '  '
+   puts "## Beginning of test: Google search"
+   puts "  "
   
-   puts 'Step 1: go to the test site: ' + testSite
+   puts "Step 1: go to the test site: " + testSite
    $ie.goto(testSite)
-   puts '  Action: entered ' + testSite + 'in the address bar.'
+   puts "  Action: entered " + testSite + "in the address bar."
 
-   puts 'Step 2: enter "pickaxe" in the search text field'
+   puts "Step 2: enter 'pickaxe' in the search text field"
    $ie.textField(:name, "q").set("pickaxe")       # q is the name of the search field
-   puts '  Action: entered pickaxe in the search field'
+   puts "  Action: entered pickaxe in the search field"
 
-   puts 'Step 3: click the "Google Search" button'
+   puts "Step 3: click the 'Google Search' button"
    $ie.button(:caption, "Google Search").click   # Google Search is the caption of the button
-   puts '  Action: clicked the Google Search button.'
+   puts "  Action: clicked the Google Search button."
 
-   puts 'Expected Result: '
-   puts ' - a google page with results should be shown. "Pragmatic Programmers LLC" should be high on the list.'
+   puts "Expected Result: "
+   puts " - a google page with results should be shown. 'Pragmatic Programmers LLC' should be high on the list."
   
-   puts 'Actual Result: Check that the "The Pragmatic Programmers, LLC" link appears on the results page '
+   puts "Actual Result: Check that the 'The Pragmatic Programmers, LLC' link appears on the results page "
    a = $ie.pageContainsText("The Pragmatic Programmers, LLC") 
    if !a 
-      puts "Test Failed! Could not find test string: The Pragmatic Programmers, LLC" 
+      puts "Test Failed! Could not find: 'The Pragmatic Programmers, LLC'" 
    else
-      puts "Test Passed. Found the test string: The Pragmatic Programmers, LLC. Actual Results match Expected Results."
+      puts "Test Passed. Found the test string: 'The Pragmatic Programmers, LLC'. Actual Results match Expected Results."
    end
    
-   puts '  '
-   puts '## End of test: google search'
+   puts "  "
+   puts "## End of test: Google search"
   
 
 # -end of simple google search test
