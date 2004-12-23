d7df4e7ae65e448452e5105711a59fc221ddad3f
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2004-12-23 00:07:08 +0000

    spaces allowed in the path name

--- watir/unittests/popups_test.rb
+++ watir/unittests/popups_test.rb
@@ -1,6 +1,8 @@
 # tests for Buttons
 # revision: $Revision$
 
+
+
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
 require 'watir'
 require 'test/unit'
@@ -8,35 +10,41 @@ require 'test/unit/ui/console/testrunner'
 require 'testUnitAddons'
 require 'unittests/setup'
 
+
+
 class TC_PopUps < Test::Unit::TestCase
 
     def gotoPopUpPage()
         $ie.goto("file://#{$myDir}/html/popups1.html")
     end
 
+    def startClicker( button , waitTime = 3)
+
+        w = WinClicker.new
+        longName = $ie.dir.gsub("/" , "\\" )
+        shortName = w.getShortFileName(longName)
+        c = "start #{shortName }\\clickJSDialog.rb #{button } #{ waitTime} "
+        puts "Starting #{c}"
+        w.winsystem(c )   
+        w=nil
+
+
+    end
+
 
     def test_simple
         gotoPopUpPage()
-        c = "start #{$ie.dir.gsub("/" , "\\" )}\\clickJSDialog.rb OK 3"
-        puts "Starting #{c}"
-        w = WinClicker.new
-        w.winsystem(c )   
+        startClicker("OK" , 3)
         $ie.button("Alert").click
     end
 
     def test_confirm
         gotoPopUpPage()
-        c = "start #{$ie.dir.gsub("/" , "\\" )}\\clickJSDialog.rb OK 3"
-        puts "Starting #{c}"
-        w = WinClicker.new
-        w.winsystem(c )   
+        startClicker("OK" , 3)
         $ie.button("Confirm").click
         assert( $ie.textField(:name , "confirmtext").verify_contains("OK") )
 
-        c = "start #{$ie.dir.gsub("/" , "\\" )}\\clickJSDialog.rb Cancel 3"
-        puts "Starting #{c}"
-        w = WinClicker.new
-        w.winsystem(c )   
+        startClicker("Cancel" , 3)
         $ie.button("Confirm").click
         assert( $ie.textField(:name , "confirmtext").verify_contains("Cancel") )
 
@@ -47,10 +55,7 @@ class TC_PopUps < Test::Unit::TestCase
 
     def atest_Prompt
         gotoPopUpPage()
-        c = "start #{$ie.dir.gsub("/" , "\\" )}\\clickJSDialog.rb Cancel 3  "
-        puts "Starting #{c}"
-        w = WinClicker.new
-        w.winsystem(c )   
+        startClicker("OK" , 3)
         $ie.button("Prompt").click
 
 
