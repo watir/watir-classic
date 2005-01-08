913f050cdff3d8e4b594e17f308f1bef087c7e0f
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-08 22:43:46 +0000

    new example demonstrates concurrent testing, developed with chris mcmahon

---
+++ examples/concurrent_search.rb
@@ -0,0 +1,21 @@
+# demonstrate ability to run multiple tests concurrently
+
+require 'thread'
+require 'watir'  
+
+def test_google
+   testSite = 'http://www.google.com'
+   ie = IE.new
+   ie.goto(testSite)
+   ie.textField(:name, "q").set("pickaxe")    
+   ie.button(:value, "Google Search").click   
+   ie.close
+end
+
+threads = []
+3.times do
+  threads << Thread.new do
+    test_google
+  end
+end
+threads.each {|x| x.join}
