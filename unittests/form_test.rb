20c218c73e88879ad9560aa656ab3df963c43b34
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-15 10:28:53 +0000

    combined all form tests into one file

--- unittests/form_test.rb
+++ unittests/form_test.rb
@@ -84,3 +84,157 @@ class TC_Forms1 < Test::Unit::TestCase
        assert($ie.checkBox(:name, "box4" , 3).isSet?)   
     end
 end
+
+class TC_Forms2 < Test::Unit::TestCase
+    include Watir
+    
+    def setup()
+        $ie.goto($htmlRoot + "forms2.html")
+    end
+    
+    def test_Form_Exists
+        assert($ie.form(:name, "test2").exists?)   
+        assert_false($ie.form(:name, "missing").exists?)   
+        
+        assert($ie.form("test2").exists?)   
+        assert_false($ie.form( "missing").exists?)   
+        
+        assert($ie.form(:index,  1).exists?)   
+        assert_false($ie.form(:index, 88).exists?)   
+        
+        assert($ie.form(:method, "get").exists?)   
+        assert_false($ie.form(:method, "missing").exists?)   
+        
+        assert($ie.form(:action, "pass.html").exists?)   
+        assert_false($ie.form(:action, "missing").exists?)   
+    end
+    
+    def xtest_showforms
+        gotoFormsPage()
+        puts"--------------------------- forms----------------------"
+        $ie.showForms
+    end
+    
+    def test_ButtonInForm
+        assert($ie.form(:name ,"test2").button(:caption , "Submit").exists?)
+    end 
+    
+end
+
+class TC_Forms3 < Test::Unit::TestCase
+    include Watir
+
+    def gotoFormsPage()
+        $ie.goto($htmlRoot + "forms3.html")
+    end
+
+    def test_Form_Exists
+       gotoFormsPage()
+
+       assert($ie.form(:name, "test2").exists?)   
+       assert_false($ie.form(:name, "missing").exists?)   
+
+       assert($ie.form("test2").exists?)   
+       assert_false($ie.form( "missing").exists?)   
+
+       assert($ie.form(:index,  1).exists?)   
+       assert_false($ie.form(:index, 88).exists?)   
+
+       assert($ie.form(:method, "get").exists?)   
+       assert_false($ie.form(:method, "missing").exists?)   
+
+       assert($ie.form(:action, "pass.html").exists?)   
+       assert_false($ie.form(:action, "missing").exists?)   
+    end
+
+    def test_showforms
+       gotoFormsPage()
+       puts"--------------------------- forms----------------------"
+        $ie.showForms
+    end
+
+    def test_Flash
+       gotoFormsPage()
+
+        $ie.form(:name ,"test2").button(:caption , "Submit").flash
+    end 
+
+    def test_objects_with_same_name
+        gotoFormsPage()
+        assert_equal('textfield' ,$ie.textField( :name , 'g1').getProperty('value') )
+        assert_equal('button'    ,$ie.button(    :name , 'g1').getProperty('value') )
+        assert_equal('1'         ,$ie.checkBox(  :name , 'g1').getProperty('value') )
+        assert_equal('2'         ,$ie.radio(     :name , 'g1').getProperty('value') )
+
+        assert_equal('textfield_id' ,$ie.textField( :id , 'g1').getProperty('value') )
+        assert_equal('button_id'    ,$ie.button(    :id , 'g1').getProperty('value') )
+        assert_equal('1_id'         ,$ie.checkBox(  :id , 'g1').getProperty('value') )
+        assert_equal('2_id'         ,$ie.radio(     :id , 'g1').getProperty('value') )
+
+
+
+    end
+
+
+
+    def test_flash
+        $ie.button( 'Click Me').flash
+        assert_raises( UnknownObjectException ) { $ie.textField( :name , 'g177').flash   }
+    end
+
+    def test_showElements
+        $ie.showAllObjects
+    end
+
+    def test_showText
+       puts $ie.getText
+    end
+
+    def test_showHTML
+       puts $ie.getHTML
+    end
+
+
+    def test_submitWithImage
+
+        assert( $ie.button(:alt , "submit").exists? )
+        assert( $ie.button(:alt , /sub/).exists? )
+
+        assert_false( $ie.button(:alt , "missing").exists? )
+        assert_false( $ie.button(:alt , /missing/).exists? )
+
+        #assert( $ie.button(:src , "file:///#{$myDir}/html/images/button.jpg").exists? )    # this doesnt work for everybody
+        assert( $ie.button(:src , /button/).exists? )
+
+        assert_false( $ie.button(:src , "missing").exists? )
+        assert_false( $ie.button(:src , /missing/).exists? )
+        assert_nothing_raised("raised an exception when it shouldnt have") { $ie.button(:src , /button/).click }
+
+        assert( $ie.pageContainsText("PASS") )
+
+    end
+
+
+end
+
+class TC_Forms4 < Test::Unit::TestCase
+    include Watir
+    
+    def setup()
+        $ie.goto($htmlRoot + "forms4.html")
+    end
+    
+    def test_find_text_field_ignoring_form
+        assert_equal($ie.textField(:name, 'name').getContents, 'apple') # should it raise a not-unique error instead?
+    end
+    
+    def test_correct_form_field_is_found_using_form_name
+        assert_equal($ie.form(:name, 'apple_form').textField(:name, 'name').getContents, 'apple')
+        assert_equal($ie.form(:name, 'banana_form').textField(:name, 'name').getContents, 'banana')
+    end
+
+    def test_correct_form_field_is_found_using_form_index
+        assert_equal($ie.form(:index, 1).textField(:name, 'name').getContents, 'apple')
+        assert_equal($ie.form(:index, 2).textField(:name, 'name').getContents, 'banana')
+    end
+end
