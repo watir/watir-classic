71dbca180dc6a6da5ebc948843631994a753e9c9
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-15 05:05:48 +0000

    removed test that didn't test; created setup method

--- unittests/form2_test.rb
+++ unittests/form2_test.rb
@@ -6,45 +6,36 @@ require 'unittests/setup'
 
 class TC_Forms < Test::Unit::TestCase
     include Watir
-
-    def gotoFormsPage()
+    
+    def setup()
         $ie.goto($htmlRoot + "forms2.html")
     end
-
-
+    
     def test_Form_Exists
-       gotoFormsPage()
-
-       assert($ie.form(:name, "test2").exists?)   
-       assert_false($ie.form(:name, "missing").exists?)   
-
-       assert($ie.form("test2").exists?)   
-       assert_false($ie.form( "missing").exists?)   
-
-       assert($ie.form(:index,  1).exists?)   
-       assert_false($ie.form(:index, 88).exists?)   
-
-       assert($ie.form(:method, "get").exists?)   
-       assert_false($ie.form(:method, "missing").exists?)   
-
-       assert($ie.form(:action, "pass.html").exists?)   
-       assert_false($ie.form(:action, "missing").exists?)   
-
-
-
-       
+        assert($ie.form(:name, "test2").exists?)   
+        assert_false($ie.form(:name, "missing").exists?)   
+        
+        assert($ie.form("test2").exists?)   
+        assert_false($ie.form( "missing").exists?)   
+        
+        assert($ie.form(:index,  1).exists?)   
+        assert_false($ie.form(:index, 88).exists?)   
+        
+        assert($ie.form(:method, "get").exists?)   
+        assert_false($ie.form(:method, "missing").exists?)   
+        
+        assert($ie.form(:action, "pass.html").exists?)   
+        assert_false($ie.form(:action, "missing").exists?)   
     end
-
-    def test_showforms
-       gotoFormsPage()
-       puts"--------------------------- forms----------------------"
+    
+    def xtest_showforms
+        gotoFormsPage()
+        puts"--------------------------- forms----------------------"
         $ie.showForms
     end
-
+    
     def test_ButtonInForm
-       gotoFormsPage()
-
         assert($ie.form(:name ,"test2").button(:caption , "Submit").exists?)
     end 
-
+    
 end
