309c3b143c47e54bfdd460acf21e0a1b6a4f38be
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-25 16:01:18 +0000

    form refactorings; reorg frame tests

--- unittests/frame_test.rb
+++ unittests/frame_test.rb
@@ -7,44 +7,50 @@ require 'unittests/setup'
 class TC_Frames < Test::Unit::TestCase
     include Watir
 
-    def gotoFramesPage()
-       $ie.clearFrame
-       $ie.clearPresetFrame
-       $ie.goto($htmlRoot + "frame_buttons.html")
+    def setup()
+        $ie.clearFrame
+        $ie.clearPresetFrame
+        $ie.goto($htmlRoot + "frame_buttons.html")
     end
 
-
     def test_frame
-        gotoFramesPage()
-
-       $ie.showFrames
-
-       assert_raises(UnknownFrameException, "UnknownFrameExceptionwas supposed to be thrown" ) {   $ie.frame("missingFrame").button(:id, "b2").enabled?  }  
-       assert_raises(UnknownObjectException, "UnknownObjectException supposed to be thrown" ) {   $ie.frame("buttonFrame2").button(:id, "b2").enabled?  }  
-
-       assert($ie.frame("buttonFrame").button(:id, "b2").enabled?)   
-
+        $ie.showFrames
+        assert_raises(UnknownFrameException) { $ie.frame("missingFrame").button(:id, "b2").enabled?  }  
+        assert_raises(UnknownObjectException) { $ie.frame("buttonFrame2").button(:id, "b2").enabled?  }  
+        assert($ie.frame("buttonFrame").button(:id, "b2").enabled?)   
     end
 
     def test_presetFrame
-
         # with the preset frame functionality we are able to use the same frame for saeveral actions
-
-        gotoFramesPage()
-
         $ie.presetFrame( "buttonFrame" )
         assert_equal( "buttonFrame" , $ie.getCurrentFrame )
-
-
         assert($ie.button(:id, "b2").enabled?)
         assert_false($ie.button(:caption, "Disabled Button").enabled?)
-
         $ie.clearPresetFrame( )
         assert_equal( "" , $ie.getCurrentFrame )
-
-
-
     end
 
 end
+
+class TC_NestedFrames < Test::Unit::TestCase
+    include Watir
+   
+    def setup()
+        $ie.clearFrame
+        $ie.clearPresetFrame
+        $ie.goto($htmlRoot + "nestedFrames.html")
+    end
+
+    def test_frame
+        $ie.showFrames
+        assert_raises(UnknownFrameException) { $ie.frame("missingFrame").button(:id, "b2").enabled?  }  
+        assert_raises(UnknownFrameException) { $ie.frame("nestedFrame").frame("subFrame").button(:id, "b2").enabled?  }  
+        assert($ie.frame("nestedFrame").frame("senderFrame").button(:name, "sendIt").enabled?)   
+        $ie.frame("nestedFrame").frame("senderFrame").textField(:index , "1" ).set("Hello")
+        $ie.frame("nestedFrame").frame("senderFrame").button(:name, "sendIt").click()
+        assert($ie.frame("nestedFrame").frame("receiverFrame").textField(:name, "receiverText").verify_contains("Hello"))   
+    end
+
+end
+
 
