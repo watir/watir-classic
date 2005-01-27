f8066f188c35c13d9ff1b75ff3b062c22ff261fa
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-27 03:13:21 +0000

    new test for divs

---
+++ watir/unittests/div_test.rb
@@ -0,0 +1,40 @@
+# feature tests for Divs
+# revision: $Revision$
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
+require 'unittests/setup'
+
+class TC_Divs < Test::Unit::TestCase
+    include Watir
+
+    def setup()
+        $ie.goto($htmlRoot + "div.html")
+    end
+
+
+    def test_Divs
+
+       assert_raises(UnknownObjectException) {$ie.div(:id , "div77").click }
+
+       assert($ie.textField(:name, "text1").verify_contains("0") )  
+       $ie.div(:id , "div3").click
+       assert($ie.textField(:name, "text1").verify_contains("1") )  
+
+       $ie.div(:id , "div4").click
+       assert($ie.textField(:name, "text1").verify_contains("0") )  
+    end
+
+    def test_getText
+       assert_raises(UnknownObjectException) {$ie.div(:id , "div77").getText }
+       assert_equal("This div has an onClick that increments text1" ,   $ie.div(:id , "div3").getText.strip )
+    end
+
+    def test_getStyle
+
+        assert_raises(UnknownObjectException) {$ie.div(:id , "div77").getStyle }
+        assert_equal("blueText" ,   $ie.div(:id , "div2").getStyle )
+        assert_equal("" ,   $ie.div(:id , "div1").getStyle )
+
+    end
+
+end
