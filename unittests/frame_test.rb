232361c1da31878c69428be498096bad409f4e39
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-29 03:37:58 +0000

    removed FrameHandler, added Frame

--- unittests/frame_test.rb
+++ unittests/frame_test.rb
@@ -8,26 +8,24 @@ class TC_Frames < Test::Unit::TestCase
     include Watir
 
     def setup()
-        $ie.clearFrame
-        $ie.clearPresetFrame
         $ie.goto($htmlRoot + "frame_buttons.html")
     end
 
     def test_frame
-        $ie.showFrames
         assert_raises(UnknownFrameException) { $ie.frame("missingFrame").button(:id, "b2").enabled?  }  
         assert_raises(UnknownObjectException) { $ie.frame("buttonFrame2").button(:id, "b2").enabled?  }  
         assert($ie.frame("buttonFrame").button(:id, "b2").enabled?)   
+        assert_false($ie.frame("buttonFrame").button(:caption, "Disabled Button").enabled?)
     end
 
     def test_presetFrame
-        # with the preset frame functionality we are able to use the same frame for saeveral actions
-        $ie.presetFrame( "buttonFrame" )
-        assert_equal( "buttonFrame" , $ie.getCurrentFrame )
-        assert($ie.button(:id, "b2").enabled?)
-        assert_false($ie.button(:caption, "Disabled Button").enabled?)
-        $ie.clearPresetFrame( )
-        assert_equal( "" , $ie.getCurrentFrame )
+        # with ruby's instance_eval, we are able to use the same frame for several actions
+        results = $ie.frame("buttonFrame").instance_eval do [
+            button(:id, "b2").enabled?, 
+            button(:caption, "Disabled Button").enabled?
+            ]
+        end
+        assert_equal([true, false], results)
     end
 
 end
@@ -36,13 +34,10 @@ class TC_NestedFrames < Test::Unit::TestCase
     include Watir
    
     def setup()
-        $ie.clearFrame
-        $ie.clearPresetFrame
         $ie.goto($htmlRoot + "nestedFrames.html")
     end
 
     def test_frame
-        $ie.showFrames
         assert_raises(UnknownFrameException) { $ie.frame("missingFrame").button(:id, "b2").enabled?  }  
         assert_raises(UnknownFrameException) { $ie.frame("nestedFrame").frame("subFrame").button(:id, "b2").enabled?  }  
         assert($ie.frame("nestedFrame").frame("senderFrame").button(:name, "sendIt").enabled?)   
@@ -52,5 +47,58 @@ class TC_NestedFrames < Test::Unit::TestCase
     end
 
 end
+
+class TC_IFrames < Test::Unit::TestCase
+    include Watir
+
+    def setup()
+        $ie.goto($htmlRoot + "iframeTest.html")
+    end
+
+    def test_Iframe
+       $ie.frame("senderFrame").textField(:name , "textToSend").set( "Hello World")
+       $ie.frame("senderFrame").button(:index, 1).click
+       assert( $ie.frame("receiverFrame").textField(:name , "receiverText").verify_contains("Hello World") )
+    end
+
+end   
+
+require 'unittests/iostring'
+class TC_show_frames < Test::Unit::TestCase
+    include MockStdoutTestCase                
+    
+    def capture_and_compare(page, expected)
+        $ie.goto($htmlRoot + page)
+        $stdout = @mockout
+        $ie.showFrames
+        assert_equal(expected, @mockout)
+    end
+      
+    def test_show_nested_frames
+        capture_and_compare("nestedFrames.html", <<END_OF_MESSAGE)
+there are 2 frames
+frame  index: 0 name: nestedFrame
+frame  index: 1 name: nestedFrame2
+END_OF_MESSAGE
+    end
+
+    def test_button_frames
+        capture_and_compare("frame_buttons.html", <<END_OF_MESSAGE)
+there are 2 frames
+frame  index: 0 name: buttonFrame
+frame  index: 1 name: buttonFrame2
+END_OF_MESSAGE
+    end
+
+    def test_iframes
+        capture_and_compare("iframeTest.html", <<END_OF_MESSAGE)
+there are 2 frames
+frame  index: 0 name: senderFrame
+frame  index: 1 name: receiverFrame
+END_OF_MESSAGE
+    end
+
+end
+
 
 
