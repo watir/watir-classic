13aa3080ea9c37b6aa97ac6f089e00e2cae890d1
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-01-30 02:08:56 +0000

    minor changes

--- watir/examples/articleExample.rb
+++ watir/examples/articleExample.rb
@@ -4,18 +4,12 @@ require 'test/unit'
 
 class TC_article_example < Test::Unit::TestCase
  
- def test_search
- 
-   ie = IE.new
-
-   ie.goto("http://www.google.com")
-
-   ie.textField(:name, "q").set("pickaxe")
-
-   ie.button(:value, "Google Search").click
-
-   assert(ie.pageContainsText("Programming Ruby, 2nd Ed."))
-
- end
+  def test_search
+    ie = IE.new
+    ie.goto("http://www.google.com")
+    ie.textField(:name, "q").set("pickaxe")
+    ie.button(:value, "Google Search").click
+    assert(ie.pageContainsText("Programming Ruby, 2nd Ed."))
+  end
 
 end
