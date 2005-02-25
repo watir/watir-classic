092cf7b58c59e67dc4514e9ea2552991fa04cecc
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-02-25 19:10:34 +0000

    more minor improvements

--- scripting101/solutions/lab2.rb
+++ scripting101/solutions/lab2.rb
@@ -1,17 +1,32 @@
 # Suggested solution to lab 2 using watir.
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..')
+
 require 'watir'
+require 'toolkit/testhook'
+
 include Watir
+
+user_name = 'paul'
+
+# make sure we have a new user
+ensure_no_user_data( user_name )
 
 # 1. Login using existing account
 $ie = IE.new
 $ie.goto('http://localhost:8080')
-$ie.textField(:name, 'name').set('paul')
-$ie.form(:index, 1).submit
+$ie.textField(:name, 'name').set(user_name)
+$ie.button(:value , 'Login').click
+
+
+# if there is no submit button available on a form, the form can be submitted like this:
+#$ie.form(:index, 1).submit
 
 # 2. Create a new job
 $ie.form(:action, 'job').textField(:name, 'name').set('ruby article')
-$ie.form(:action, 'job').submit
+
+# here we use a regular expression to locate the create button
+$ie.button(:value , /create/i ).click 
 
 # 3. Start the new job
 $ie.button(:value, 'ruby article').click
--- scripting101/solutions/lab3_2.rb
+++ scripting101/solutions/lab3_2.rb
@@ -5,26 +5,31 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..')
 
 require 'watir'
-require '../toolkit/testhook'
+require 'toolkit/testhook'
 
 include Watir
 
+user_name = 'ruby'
+
 # make sure we have a new user
-ensure_no_user_data 'ruby'
+ensure_no_user_data( user_name )
 
 # login
 $ie = IE.new
 $ie.goto('http://localhost:8080')
-$ie.textField(:name, 'name').set('ruby')
-$ie.form(:index, 1).submit
+$ie.textField(:name, 'name').set( user_name )
+# here we use a regular expression to locate the create button
+$ie.button(:value , 'Login').click
 
 # create a background job
 $ie.form(:action, 'job').textField(:name, 'name').set('background')
-$ie.form(:action, 'job').submit
+# here we use a regular expression to locate the create button
+$ie.button(:value , /create/i ).click 
 
 # create a non-background job
 $ie.form(:action, 'job').textField(:name, 'name').set('foreground')
-$ie.form(:action, 'job').submit
+# here we use a regular expression to locate the create button
+$ie.button(:value , /create/i ).click 
 
 # start the job
 $ie.form(:action, 'start').button(:value, 'foreground').click
