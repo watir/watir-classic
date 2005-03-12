458d4a75b73cb950b4bd81346312c4970469c81a
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-12 06:47:00 +0000

    Remove references to IEC and old examples

--- wtr/scripting101/toolkit/suite.rb
+++ wtr/scripting101/toolkit/suite.rb
@@ -1,8 +1,4 @@
 # don't require a server
 require 'iostring-tests.rb' 
 require 'table-array-tests.rb'   
-require 't-interface.rb' # tests form() in iec-assist.rb (should be renamed)
-
-# requires a server to be running
-require 'iec-assist-tests.rb' # tests show_elements() in iec-assist.rb
 
