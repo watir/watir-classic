0dab840591f728125164386b609d7161ceff9584
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-13 03:09:01 +0000

    added unit tests for properties

--- unittests/links_test.rb
+++ unittests/links_test.rb
@@ -11,12 +11,12 @@ class TC_Links < Test::Unit::TestCase
         $ie.goto($htmlRoot + "links1.html")
     end
 
+    def setup()
+        gotoLinksPage()
+    end
+
     
-
-   
-
     def test_Link_Exists
-       gotoLinksPage()
        assert($ie.link(:text, "test1").exists?)   
        assert($ie.link(:text, /TEST/i).exists?)   
        assert_false($ie.link(:text, "missing").exists?)   
@@ -45,36 +45,51 @@ class TC_Links < Test::Unit::TestCase
 
     def test_Link_click
 
-       gotoLinksPage()
 
         $ie.link(:text, "test1").click
         assert( $ie.contains_text("Links2-Pass") ) 
 
-       gotoLinksPage()
+        gotoLinksPage()
 
         $ie.link(:url, "link_pass.html").click
         assert( $ie.contains_text("Links3-Pass") ) 
 
-       gotoLinksPage()
+        gotoLinksPage()
 
         $ie.link(:index, 1).click
         assert( $ie.contains_text("Links2-Pass") ) 
 
-       gotoLinksPage()
-
-        
+        gotoLinksPage()
         assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).click }  
+    end
+
+    def test_link_properties
+        
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).href }  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).value}  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).innerText }  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).name }  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).id }  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).disabled }  
+        assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.link(:index, 199).type }  
 
 
+        assert_match( /links2/ ,$ie.link(:index, 1).href )
+        assert_equal( ""      , $ie.link(:index, 1).value)
+        assert_equal( "test1" , $ie.link(:index, 1).innerText )
+        assert_equal( ""      , $ie.link(:index, 1).name )
+        assert_equal( ""      , $ie.link(:index, 1).id )
+        assert_equal( false   , $ie.link(:index, 1).disabled )  
+        assert_equal( "link"  , $ie.link(:index, 1).type )
+
+        assert_equal( "link_id"   , $ie.link(:index, 6).id )
+        assert_equal( "link_name" , $ie.link(:index, 7).name )
 
 
     end
 
     def test_showLinks
-
-        gotoLinksPage()
         $ie.showLinks
-
     end
 
 
