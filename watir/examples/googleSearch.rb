d73a2142810fae86231fc4c9c5c97ab71820864b
jonathankohl <jonathankohl@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-21 21:03:20 +0000

    removed $ie, replaced with ie

--- watir/examples/googleSearch.rb
+++ watir/examples/googleSearch.rb
@@ -20,28 +20,28 @@
    test_site = 'http://www.google.com'
   
    #open the IE browser
-   $ie = IE.new
+   ie = IE.new
 
    puts "## Beginning of test: Google search"
    puts "  "
   
    puts "Step 1: go to the test site: " + test_site
-   $ie.goto(test_site)
+   ie.goto(test_site)
    puts "  Action: entered " + test_site + " in the address bar."
 
    puts "Step 2: enter 'pickaxe' in the search text field"
-   $ie.text_field(:name, "q").set("pickaxe")       # q is the name of the search field
+   ie.text_field(:name, "q").set("pickaxe")       # q is the name of the search field
    puts "  Action: entered pickaxe in the search field"
 
    puts "Step 3: click the 'Google Search' button"
-   $ie.button(:name, "btnG").click   # "btnG" is the name of the Search button
+   ie.button(:name, "btnG").click   # "btnG" is the name of the Search button
    puts "  Action: clicked the Google Search button."
 
    puts "Expected Result: "
    puts " - a Google page with results should be shown. 'Programming Ruby' should be high on the list."
   
    puts "Actual Result: Check that the 'Programming Ruby' link appears on the results page "
-   a = $ie.contains_text("Programming Ruby") 
+   a = ie.contains_text("Programming Ruby") 
    if !a 
       puts "Test Failed! Could not find: 'Programming Ruby'" 
    else
