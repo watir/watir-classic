6e086af487f9da9a4787f140c24b92fd53e50cf1
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-28 21:43:14 +0000

    misc updates based on bangalore class

--- scripting101/toolkit/timeclock-recent-records.rb
+++ scripting101/toolkit/timeclock-recent-records.rb
@@ -42,12 +42,12 @@ def get_results_table_array (document=$ie.document)
   result
 end
 
-def assert_total_job_records( n ) 
-  assert_equal n+1, get_results_table_array.length
+def assert_total_job_records( n , document=$ie.document) 
+  assert_equal n+1, get_results_table_array(document).length
 end
 
-def assert_job_record( index, name, status )
-  results = get_results_table_array
+def assert_job_record( index, name, status, document=$ie.document)
+  results = get_results_table_array(document)
   assert_equal name, results.job_name( index )
   assert_equal status, results.status( index )
 end
