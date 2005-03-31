bea261b19b6fecc1ea91bb40dae06a42ed0c1265
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-31 03:17:09 +0000

    copy of the readme.txt file, with a class declaration, to get round rdoc bug

---
+++ readme.rb
@@ -0,0 +1,61 @@
+=begin rdoc
+
+This is Watir - Web Application Testing In Ruby
+
+To Install:
+   Execute install.rb.
+
+How To Use:
+   This only works on Windows.
+   This has been developed and tested using Ruby 1.8.1-11, Ruby 1.8.1-12  using Windows 2000 and XP.
+   It will NOT work with Ruby 1.8.1-13. (This version of Ruby has a bad WIN32OLE library.)
+   Best is to use Ruby 1.8.2-14.
+   Check out the mail lists and the documentation for the workarounds.
+   Install ruby from http://ruby-lang.org
+   Install the watir files, unit tests and sample html pages. 
+   Run the unittests in a cmd shell. Go to the dir where you installed it and then type 'ruby unittests/all_tests.rb'.
+   See the user guide if you are having problems with security blocking.
+
+Things Still to be Done:
+   waitForIE may change to use the events from Internet Explorer. This will help detect page changes that are very quick ( from a local file system)
+   RDoc - I always need to add documentation to the classes and methods
+   Javascript pop ups
+   New browser windows - experimental at the moment
+
+Contacts:
+   Paul Rogers (paul.rogers@shaw.ca)
+   Bret Pettichord (bret@pettichord.com)
+   The mailing list: http://rubyforge.org/mail/?group_id=104
+
+
+Contributors:
+   Bret Pettichord
+   Paul Rogers
+   Jonathan Kohl
+   Chris Morris
+   Karlin Fox
+   Lorenzo Jorquera
+   Atilla Ozgur
+   Justin McCarthy
+   Chris McMahon
+   Elisabeth Hendrickson
+   Michael Kelly
+   Peter Chau
+   Danny Faught
+
+
+Acknowledgements:
+   Chris Morris
+   Brian Marick
+   Jonathan Kohl
+   Penny Tonita
+   Janet Gregory
+   Andy Tinkham
+   Jacinda Scott (logo creator)
+
+   Thanks for your ideas and support!
+
+
+=end
+class ReadMe
+end
