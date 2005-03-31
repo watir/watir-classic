5036d7fbfff95a9cf5dd4187f64f201de2225ca2
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-03-31 03:15:57 +0000

    workaround for rdoc bugs

--- build-rdoc.bat
+++ build-rdoc.bat
@@ -1,1 +1,4 @@
-rdoc -m ReadMe --title "Watir API Reference" -o rdoc  ./readme.rb ./watir.rb   watir/clickJSDialog.rb watir/cookiemanager.rb watir/exceptions.rb watir/winClicker.rb watir/watir_simple.rb  
+
+
+rdoc -m ReadMe --title "Watir API Reference" -o rdoc  --exclude "unittests|examples|install.rb" 
+
