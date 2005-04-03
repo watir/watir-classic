d7ca01264bdace42684ac04bcd77d7fc18260776
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-03 20:17:50 +0000

    update for 1.2 release

--- watir/readme.rb
+++ watir/readme.rb
@@ -1,9 +1,10 @@
 =begin rdoc
 
-This is Watir - Web Application Testing In Ruby
+This is Watir - Web Application Testing In Ruby    http://wtr.rubyforge.org
+
 
 To Install:
-   Execute install.rb.
+   Execute install.rb. 
 
 How To Use:
    This only works on Windows.
@@ -15,6 +16,38 @@ How To Use:
    Install the watir files, unit tests and sample html pages. 
    Run the unittests in a cmd shell. Go to the dir where you installed it and then type 'ruby unittests/all_tests.rb'.
    See the user guide if you are having problems with security blocking.
+
+
+Typical Usage
+    # include the controller 
+   require 'watir' 
+   include Watir
+   # create an instance of the controller 
+   ie = Watir::IE.new  
+   # go to the page you want to test 
+   ie.goto("http://myserver/mypage") 
+   # to enter text into a text field - assuming the field is name "username" 
+   ie.text_field(:name, "username").set("Paul") 
+   # if there was a text field that had an id of "company_ID", you could set it to Ruby Co: 
+   ie.text_field(:id ,"company_ID").set("Ruby Co") 
+   # to click a button that has a caption of 'Cancel' 
+   ie.button(:value, "Cancel").click 
+   
+  The ways that are available to identify an html object depend upon the object type, but include
+   :id           used for an object that has an ID attribute -- this is the best way!
+   :name         used for an object that has a name attribute. 
+   :value        value of text fields, captions of buttons 
+   :index        finds the nth object of the specified type - eg button(:index , 2) finds the second button. This is 1 based. <br>
+   :beforeText   finds the object immeditaley before the specified text. Doesnt work if the text is in a table cell
+   :afterText    finds the object immeditaley after the specified text. Doesnt work if the text is in a table cell
+
+
+ command line options:
+
+  -b  (background)   Run Internet Explorer minimised
+  -s  (Spinner off)  Use this when you dont want the spinner displyed. Most usful when using an ide like eclipse or scite
+
+
 
 Things Still to be Done:
    waitForIE may change to use the events from Internet Explorer. This will help detect page changes that are very quick ( from a local file system)
