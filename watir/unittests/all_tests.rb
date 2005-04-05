e53878414840e0382c6e892678eec0651733f8f9
(no author) <(no author)@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-05 03:04:04 +0000

    This commit was manufactured by cvs2svn to create tag 'before_delete'.

--- watir/unittests/all_tests.rb
+++
@@ -1,5 +0,0 @@
-TOPDIR = File.join(File.dirname(__FILE__), '..')
-$LOAD_PATH.unshift TOPDIR
-Dir.chdir TOPDIR
-(Dir["unittests/*_test.rb"] - 
-  (["unittests/popups_test.rb"] + ["unittests/filefield_test.rb"])).each {|x| require x}
--- wtr/scripting101/timeclock/ruby-trace/ruby-trace/foo.rb
+++
@@ -1,3 +0,0 @@
-class Foo
-  $trace.topic('foo').methods_on_class(Foo)
-end
