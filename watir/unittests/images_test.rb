987d6473b5b1d9fb1358046c71112cc0676b7ad8
sipea <sipea@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-21 21:05:32 +0000

    added test methods for the save method of the Image class

--- watir/unittests/images_test.rb
+++ watir/unittests/images_test.rb
@@ -3,6 +3,9 @@
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
 require 'unittests/setup'
+require 'ftools'
+require 'webrick'
+require 'watir/cookiemanager'
 
 class TC_Images < Test::Unit::TestCase
     include Watir
@@ -132,6 +135,98 @@ class TC_Images < Test::Unit::TestCase
         end
         assert_equal( index-1 , $ie.images.length )
 
+    end
+
+    def test_save_local_image
+        gotoImagePage()
+        safe_file_block(1) {
+            assert(File.compare("sample.img.dat", $ie.images[1].src.gsub(/^file:\/\/\//, '')))
+        }
+    end
+
+    def test_save_local_image_overwrites
+        gotoImagePage()
+        safe_file_block(1,2) {
+            assert(File.compare("sample.img.dat", $ie.images[2].src.gsub(/^file:\/\/\//, '')))
+        }
+    end
+
+    def test_save_remote_image
+        run_webrick {
+            goto_local_served_page
+            safe_file_block(1) {
+                assert(File.compare("sample.img.dat", build_path("html/images/triangle.jpg")))
+            }
+        }
+    end
+
+    def test_save_remote_image_overwrites
+        run_webrick {
+            goto_local_served_page
+            safe_file_block(1,2) {
+                assert(File.compare("sample.img.dat", build_path("html/images/square.jpg")))
+            }
+        }
+    end
+
+    def test_not_found_in_cache_throws
+        run_webrick {
+            goto_local_served_page
+            Watir::CookieManager::WatirHelper.deleteSpecialFolderContents(0x0020)
+            assert_raises(CacheItemNotFound ) { $ie.images[1].save("sample.img.dat") }
+        }
+    end
+
+    def test_most_recent_returned_when_duplicates
+        run_webrick {
+            goto_local_served_page
+        }
+        File.copy(build_path("html/images/square.jpg"), build_path("html/images/triangle.jpg"))
+        begin
+            run_webrick(34000) {
+                goto_local_served_page
+                safe_file_block(1) {
+                    assert(File.compare("sample.img.dat", build_path("html/images/square.jpg")))
+                }
+            }
+        ensure
+            File.copy(build_path("html/images/originaltriangle.jpg"), build_path("html/images/triangle.jpg"))
+        end
+    end
+
+    def goto_local_served_page
+        $ie.goto("http://localhost:#{@port}/images1.html")
+    end
+
+    def safe_file_block(*imgstosave)
+        File.unlink("sample.img.dat") if (File.exists?("sample.img.dat"))
+        begin
+            imgstosave.each {|imgidx| $ie.images[imgidx].save("sample.img.dat")}
+            yield
+        ensure
+            File.unlink("sample.img.dat") if (File.exists?("sample.img.dat"))
+        end
+    end
+    
+    def build_path(part) 
+      dir = part
+      dir = "unittests/" + dir if ($0 != __FILE__)
+      dir
+    end
+
+    def run_webrick(port = 33000)
+        @port = port
+        server = WEBrick::HTTPServer.new(:Port => port,
+                                         :DocumentRoot => build_path("html"),
+                                         :Logger => WEBrick::Log.new(nil, WEBrick::Log::FATAL ))
+        begin
+            runner = Thread.new(server) {|svr| svr.start()}
+            yield
+        ensure
+            server.stop()
+            runner.join()
+            server.shutdown()
+        end
     end
 end
 
