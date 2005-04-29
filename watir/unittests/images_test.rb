b9d6f5cace3524faec8c3ac2bcded52126150eb5
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-29 21:31:06 +0000

    removing white space and reformatting to make legible

--- watir/unittests/images_test.rb
+++ watir/unittests/images_test.rb
@@ -9,86 +9,66 @@ require 'watir/cookiemanager'
 
 class TC_Images < Test::Unit::TestCase
     include Watir
-
+    
     def setup
         gotoImagePage
     end
-
+    
     def gotoImagePage()
         $ie.goto($htmlRoot + "images1.html")
     end
-
-
-
+    
     def test_imageExists
-
-
-
         assert_false( $ie.image(:name , "missing_name").exists?  )
         assert(       $ie.image(:name , "circle").exists?  )
         assert(       $ie.image(:name , /circ/ ).exists?  )
-
-
-
+        
         assert_false( $ie.image(:id , "missing_id").exists?  )
         assert(       $ie.image(:id , "square").exists?  )
         assert(       $ie.image(:id , /squ/ ).exists?  )
-
-
+        
         assert_false( $ie.image(:src, "missingsrc.gif").exists?  )
-
-# BP -- This fails for me but not for Paul. It doesn't make sense to me that it should pass.  
-#        assert(       $ie.image(:src , "file:///#{$myDir}/html/images/triangle.jpg").exists?  )
+        
+        # BP -- This fails for me but not for Paul. It doesn't make sense to me that it should pass.  
+        #        assert(       $ie.image(:src , "file:///#{$myDir}/html/images/triangle.jpg").exists?  )
         assert(       $ie.image(:src , /triangle/ ).exists?  )
-
+        
         assert(       $ie.image(:alt , "circle" ).exists?  )
         assert(       $ie.image(:alt , /cir/ ).exists?  )
-
+        
         assert_false(  $ie.image(:alt , "triangle" ).exists?  )
         assert_false(  $ie.image(:alt , /tri/ ).exists?  )
-
-
-
     end
-
-
+    
     def test_image_click
         assert_raises(UnknownObjectException ) { $ie.image(:name, "no_image_with_this").click }
         assert_raises(UnknownObjectException ) { $ie.image(:id, "no_image_with_this").click }
         assert_raises(UnknownObjectException ) { $ie.image(:src, "no_image_with_this").click}
         assert_raises(UnknownObjectException ) { $ie.image(:alt, "no_image_with_this").click}
-
-
+        
         $ie.image(:src, /button/).click
-
         assert($ie.contains_text("PASS") )
-
-
     end
-
+    
     def test_imageHasLoaded
         assert_raises(UnknownObjectException ) { $ie.image(:name, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:id, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:src, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:alt, "no_image_with_this").hasLoaded? }
-
-
+        
         assert_false( $ie.image(:name, "themissingimage").hasLoaded?  )
         assert( $ie.image(:name, "circle").hasLoaded?  )
-
+        
         assert( $ie.image(:alt, "circle").hasLoaded?  )
         assert( $ie.image(:alt, /cir/ ).hasLoaded?  )
-
-
     end
-
+    
     def test_image_properties
-
         assert_raises(UnknownObjectException ) { $ie.image(:name, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:id, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:src, "no_image_with_this").hasLoaded? }
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).hasLoaded? }
-
+        
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).name }
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).id }
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).src }
@@ -97,7 +77,7 @@ class TC_Images < Test::Unit::TestCase
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).width }
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).fileCreatedDate }
         assert_raises(UnknownObjectException ) { $ie.image(:index, 82).fileSize }
-
+        
         assert_equal( "image"  , $ie.image(:index, 2).type ) 
         assert_equal( ""       , $ie.image(:index, 2).name ) 
         assert_equal( "square" , $ie.image(:index, 2).id )
@@ -105,24 +85,22 @@ class TC_Images < Test::Unit::TestCase
         assert_equal( "" , $ie.image(:index, 2).value )
         assert_equal( "88" , $ie.image(:index, 2).height )
         assert_equal( "88" , $ie.image(:index, 2).width )
-
+        
         # this line fails, as the date is when it is installed on the local oc, not the date the file was really created
         #assert_equal( "03/10/2005" , $ie.image(:index, 2).fileCreatedDate )
         assert_equal( "788",  $ie.image(:index, 2).fileSize )
-
-        puts"--------------------- To String tests -------------------"
-
+        
+        # to string tests -- output should be verified!
         puts $ie.image(:name  , "circle").to_s
         puts $ie.image(:index , 2).to_s
     end
-
+    
     def test_image_iterator
-
         assert_equal(6 , $ie.images.length)
         assert_equal("" , $ie.images[2].name )
         assert_equal("square" , $ie.images[2].id )
         assert_match(/square/ , $ie.images[2].src )
-
+        
         index = 1
         $ie.images.each do |i|
             assert_equal( $ie.image(:index, index).id , i.id )
@@ -130,74 +108,73 @@ class TC_Images < Test::Unit::TestCase
             assert_equal( $ie.image(:index, index).src , i.src )
             assert_equal( $ie.image(:index, index).height , i.height )
             assert_equal( $ie.image(:index, index).width , i.width )
-
+            
             index+=1
         end
         assert_equal( index-1 , $ie.images.length )
-
     end
-
+    
     def test_save_local_image
         gotoImagePage()
-        safe_file_block(1) {
+        safe_file_block(1) do
             assert(File.compare("sample.img.dat", $ie.images[1].src.gsub(/^file:\/\/\//, '')))
-        }
+        end
     end
-
+    
     def test_save_local_image_overwrites
         gotoImagePage()
-        safe_file_block(1,2) {
+        safe_file_block(1,2) do
             assert(File.compare("sample.img.dat", $ie.images[2].src.gsub(/^file:\/\/\//, '')))
-        }
+        end
     end
-
+    
     def test_save_remote_image
-        run_webrick {
+        run_webrick do
             goto_local_served_page
-            safe_file_block(1) {
+            safe_file_block(1) do
                 assert(File.compare("sample.img.dat", build_path("html/images/triangle.jpg")))
-            }
-        }
+            end
+        end
     end
-
+    
     def test_save_remote_image_overwrites
-        run_webrick {
+        run_webrick do
             goto_local_served_page
-            safe_file_block(1,2) {
+            safe_file_block(1,2) do
                 assert(File.compare("sample.img.dat", build_path("html/images/square.jpg")))
-            }
-        }
+            end
+        end
     end
-
+    
     def test_not_found_in_cache_throws
-        run_webrick {
+        run_webrick do
             goto_local_served_page
             Watir::CookieManager::WatirHelper.deleteSpecialFolderContents(0x0020)
             assert_raises(CacheItemNotFound ) { $ie.images[1].save("sample.img.dat") }
-        }
+        end
     end
-
+    
     def test_most_recent_returned_when_duplicates
-        run_webrick {
+        run_webrick do
             goto_local_served_page
-        }
+        end
         File.copy(build_path("html/images/square.jpg"), build_path("html/images/triangle.jpg"))
         begin
-            run_webrick(34000) {
+            run_webrick(34000) do
                 goto_local_served_page
-                safe_file_block(1) {
+                safe_file_block(1) do
                     assert(File.compare("sample.img.dat", build_path("html/images/square.jpg")))
-                }
-            }
+                end
+            end
         ensure
             File.copy(build_path("html/images/originaltriangle.jpg"), build_path("html/images/triangle.jpg"))
         end
     end
-
+    
     def goto_local_served_page
         $ie.goto("http://localhost:#{@port}/images1.html")
     end
-
+    
     def safe_file_block(*imgstosave)
         File.unlink("sample.img.dat") if (File.exists?("sample.img.dat"))
         begin
@@ -209,16 +186,16 @@ class TC_Images < Test::Unit::TestCase
     end
     
     def build_path(part) 
-      dir = part
-      dir = "unittests/" + dir if ($0 != __FILE__)
-      dir
+        dir = part
+        dir = "unittests/" + dir if ($0 != __FILE__)
+        dir
     end
-
+    
     def run_webrick(port = 33000)
         @port = port
         server = WEBrick::HTTPServer.new(:Port => port,
                                          :DocumentRoot => build_path("html"),
-                                         :Logger => WEBrick::Log.new(nil, WEBrick::Log::FATAL ))
+        :Logger => WEBrick::Log.new(nil, WEBrick::Log::FATAL ))
         begin
             runner = Thread.new(server) {|svr| svr.start()}
             yield
