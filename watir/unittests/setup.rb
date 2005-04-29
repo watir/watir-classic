3c9128fb465fcb7776c277acd27416ca623b7c0c
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-29 23:21:07 +0000

    speed up tests

--- watir/unittests/setup.rb
+++ watir/unittests/setup.rb
@@ -4,10 +4,18 @@ require 'test/unit'
 require 'test/unit/ui/console/testrunner'
 require 'watir/testUnitAddons'
 
+topdir = File.join(File.dirname(__FILE__), '..')
+Dir.chdir topdir
+
+$all_tests = Dir["unittests/*_test.rb"]
+$failing_tests = ["unittests/popups_test.rb"] + ["unittests/images_test.rb"]
+$noisy_tests = ['unittests/image_saveas_test.rb'] + ['unittests/screen_capture_test.rb'] + ['unittests/filefield_test.rb']
+$core_tests = $all_tests - ($failing_tests + $noisy_tests)
+
 def start_ie_with_logger
-#  logger = Watir::WatirLogger.new(File.join(File.dirname(__FILE__), 'test.txt') ,5, 65535 * 2)
   $ie = Watir::IE.new()
-#  $ie.set_logger(logger)
+  $ie.typingspeed = 0
+  $ie.defaultSleepTime = 0.01
 end
 
 def set_local_dir
@@ -21,3 +29,4 @@ end
 
 start_ie_with_logger
 set_local_dir
+
