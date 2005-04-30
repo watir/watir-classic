af6ad8971c4fda015d961921fbd01b901fa66f25
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-04-30 02:44:42 +0000

    minor cleanup

--- unittests/popups_test.rb
+++ unittests/popups_test.rb
@@ -7,12 +7,11 @@ require 'unittests/setup'
 class TC_PopUps < Test::Unit::TestCase
     include Watir
 
-    def gotoPopUpPage()
+    def setup
         $ie.goto("file://#{$myDir}/html/popups1.html")
     end
 
-    def startClicker( button , waitTime = 3)
-
+    def startClicker( button , waitTime = 0.5)
         w = WinClicker.new
         longName = $ie.dir.gsub("/" , "\\" )
         shortName = w.getShortFileName(longName)
@@ -20,39 +19,26 @@ class TC_PopUps < Test::Unit::TestCase
         puts "Starting #{c}"
         w.winsystem(c )   
         w=nil
-
-
     end
 
-
     def test_simple
-        gotoPopUpPage()
-        startClicker("OK" , 3)
+        startClicker("OK")
         $ie.button("Alert").click
     end
 
     def test_confirm
-        gotoPopUpPage()
-        startClicker("OK" , 3)
+        startClicker("OK")
         $ie.button("Confirm").click
         assert( $ie.text_field(:name , "confirmtext").verify_contains("OK") )
 
-        startClicker("Cancel" , 3)
+        startClicker("Cancel")
         $ie.button("Confirm").click
         assert( $ie.text_field(:name , "confirmtext").verify_contains("Cancel") )
-
-
-
     end
 
-
-    def atest_Prompt
-        gotoPopUpPage()
-        startClicker("OK" , 3)
+    def xtest_Prompt
+        startClicker("OK")
         $ie.button("Prompt").click
-
-
     end
-
 end
 
