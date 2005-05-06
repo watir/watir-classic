16f01823166bff7124cb1458966f497ad2bd143c
(no author) <(no author)@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-05-06 04:43:46 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_3'.

--- wtr/cliec/contrib/motunes/lib/borges/lib/Borges/RequestHandler/NotFoundHandler.rb
+++
@@ -1,8 +0,0 @@
-class Borges::NotFoundHandler < Borges::RequestHandler
-
-  def handle_request_intern(req)
-    return Borges::NotFoundResponse.new(req.path)
-  end
-
-end
-
--- wtr/scripting101/contrib/irb_helper.rb
+++
@@ -1,9 +0,0 @@
-#
-#  use this to make the testing through irb easier
-#
-
-require 'helpers'
-require 'IEController'
-require 'tc_methods'
-
-$debuglevel=1
