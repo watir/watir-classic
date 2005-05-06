6d1bd04d2cb871a6e86dc6463ebdda10df7c79d2
sipea <sipea@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-05-06 12:13:51 +0000

    added the check_autoit_installed method and minor formatting changes

--- watir/WindowHelper.rb
+++ watir/WindowHelper.rb
@@ -1,29 +1,40 @@
 require 'win32ole'
 
 class WindowHelper
-
 	def initialize( )
-		@autoit = WIN32OLE.new('AutoItX3.Control')
-        end
+		  @autoit = WIN32OLE.new('AutoItX3.Control')
+  end
+  
 	def push_alert_button()
-		@autoit.WinWait "Microsoft Internet Explorer", ""
-		@autoit.Send "{ENTER}"
+		  @autoit.WinWait "Microsoft Internet Explorer", ""
+		  @autoit.Send "{ENTER}"
 	end
+  
 	def push_confirm_button_ok()
-		@autoit.WinWait "Microsoft Internet Explorer", ""
-		@autoit.Send "{ENTER}"
+	  	@autoit.WinWait "Microsoft Internet Explorer", ""
+	  	@autoit.Send "{ENTER}"
 	end
+  
 	def push_confirm_button_cancel()
-		@autoit.WinWait "Microsoft Internet Explorer", ""
-		@autoit.Send "{ESCAPE}"
+	  	@autoit.WinWait "Microsoft Internet Explorer", ""
+	  	@autoit.Send "{ESCAPE}"
 	end
+  
 	def logon(title,name = 'john doe',password = 'john doe')
-		@autoit.WinWait title, ""
-		@autoit.Send name
-		@autoit.Send "{TAB}"
-		@autoit.Send password
-		@autoit.Send "{ENTER}"
+		  @autoit.WinWait title, ""
+	  	@autoit.Send name
+	  	@autoit.Send "{TAB}"
+	  	@autoit.Send password
+	  	@autoit.Send "{ENTER}"
 	end
+  
+  def WindowHelper.check_autoit_installed
+      begin
+          WIN32OLE.new('AutoItX3.Control')
+      rescue
+          raise Watir::Exception::WatirException, "The AutoIt dll must be correctly registered for this feature to work properly"
+      end
+  end
 end
 
 
