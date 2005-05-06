7983f47c7e4bd7b71efc8ccfed7bde07273bf5c0
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-05-06 16:30:45 +0000

    whitespace only

--- unittests/ie_mock.rb
+++ unittests/ie_mock.rb
@@ -2,9 +2,9 @@
 require 'watir'
 
 class FakeFrame
-	def length
-		return 0
-	end
+    def length
+        return 0
+    end
 end
 
 class FakeAll
@@ -15,79 +15,79 @@ class FakeAll
         @doc.links
     end
 end
-        
+
 
 class FakeDoc
-
-   attr_accessor :links, :all
-   
-   def initialize()
-      @links = ""
-      @all = FakeAll.new(self)
-   end
-   
-	def frames
-		return FakeFrame.new()
-	end
-	
-	def readyState
-		return "complete"
-	end
-	
-	def addLink(value)
-		if @links.nil?
-			@links = value
-		else
-			@links << value
-		end
-	end
- 
-      def url
-          return "file://fake"
-      end
-
+    
+    attr_accessor :links, :all
+    
+    def initialize()
+        @links = ""
+        @all = FakeAll.new(self)
+    end
+    
+    def frames
+        return FakeFrame.new()
+    end
+    
+    def readyState
+        return "complete"
+    end
+    
+    def addLink(value)
+        if @links.nil?
+            @links = value
+        else
+            @links << value
+        end
+    end
+    
+    def url
+        return "file://fake"
+    end
+    
 end
 
 class StubExplorer
-
-	attr_accessor :timeToWait
-
-	def initialize()
-		@timeToWait = 1
-		@visible = $HIDE_IE
+    
+    attr_accessor :timeToWait
+    
+    def initialize()
+        @timeToWait = 1
+        @visible = $HIDE_IE
         @document = FakeDoc.new()
-	end
-	
-	def document
-		return @document
-	end
-	
-	def busy ()
-		sleep @timeToWait
-		return FALSE
-	end
-	
-	def readyState
-		return 4
-	end
-	
-	def visible=(value)
-	end
-	
+    end
+    
+    def document
+        return @document
+    end
+    
+    def busy ()
+        sleep @timeToWait
+        return FALSE
+    end
+    
+    def readyState
+        return 4
+    end
+    
+    def visible=(value)
+    end
+    
 end
 
 class TestIE < Watir::IE
-
-   def create_browser_window
-      @ie = StubExplorer.new()
-   end
-   
-   def addLink(link)
-      @ie.document.addLink(link)
-   end
-	
-   def setTimeToWait(time = 1)
-      @ie.timeToWait = time
-   end
-	
+    
+    def create_browser_window
+        @ie = StubExplorer.new()
+    end
+    
+    def addLink(link)
+        @ie.document.addLink(link)
+    end
+    
+    def setTimeToWait(time = 1)
+        @ie.timeToWait = time
+    end
+    
 end
--- unittests/ie_test.rb
+++ unittests/ie_test.rb
@@ -8,43 +8,43 @@ require 'test/unit'
 
 class TC_ie < Test::Unit::TestCase
     include Watir
-
-   def setup
-      @faked_ie = TestIE.new
-   end
     
-   def test_waitForIE
-      wait_time = 1
-#      @faked_ie.logger.level = Logger::DEBUG
+    def setup
+        @faked_ie = TestIE.new
+    end
     
-      @faked_ie.setTimeToWait wait_time
-      begin_time = Time.now
-      @faked_ie.wait
-      elapsed_time = Time.now - begin_time
-      assert_in_delta(wait_time, elapsed_time, 0.5)
-   end
+    def test_waitForIE
+        wait_time = 1
+        #      @faked_ie.logger.level = Logger::DEBUG
+        
+        @faked_ie.setTimeToWait wait_time
+        begin_time = Time.now
+        @faked_ie.wait
+        elapsed_time = Time.now - begin_time
+        assert_in_delta(wait_time, elapsed_time, 0.5)
+    end
     
-   # is this correct? 
-   def test_getLink_ByIndexReturnsNilOnNoLinks
-      assert_nil(@faked_ie.getLink(:index, 1))
-      assert_nil(@faked_ie.getLink(:index, 1))
-   end
+    # is this correct? 
+    def test_getLink_ByIndexReturnsNilOnNoLinks
+        assert_nil(@faked_ie.getLink(:index, 1))
+        assert_nil(@faked_ie.getLink(:index, 1))
+    end
     
-   def test_getLink_ByBadHow
-      @faked_ie.addLink "foo"
-      assert_raise(MissingWayOfFindingObjectException) do
-          @faked_ie.getLink(:no_such_mechanism, "verifying error handling")
-      end
-   end
+    def test_getLink_ByBadHow
+        @faked_ie.addLink "foo"
+        assert_raise(MissingWayOfFindingObjectException) do
+            @faked_ie.getLink(:no_such_mechanism, "verifying error handling")
+        end
+    end
     
-   # is this correct? 
-   def test_getLink_ByUrlReturnsNilOnNoLinks
-      assert_nil(@faked_ie.getLink(:url, "whatever"))
-   end
+    # is this correct? 
+    def test_getLink_ByUrlReturnsNilOnNoLinks
+        assert_nil(@faked_ie.getLink(:url, "whatever"))
+    end
     
-   # is this correct? 
-   def test_getLink_ByTextReturnsNilOnNoLinks
-      assert_nil(@faked_ie.getLink(:text, "whatever"))
-   end
+    # is this correct? 
+    def test_getLink_ByTextReturnsNilOnNoLinks
+        assert_nil(@faked_ie.getLink(:text, "whatever"))
+    end
 end
 
