dd08f964637e421d6c54d22eda8086d6a7238216
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-05-12 22:13:35 +0000

    whitespace

--- watir/unittests/screen_capture_test.rb
+++ watir/unittests/screen_capture_test.rb
@@ -8,42 +8,42 @@ require 'watir/screen_capture'
 class TC_Capture< Test::Unit::TestCase
     include Watir
     include Watir::ScreenCapture
-   
-    def setup( )
-       delete_captured_files( [ 'jpeg1.jpg' , 'jpeg2.jpg' , 'bmp1.bmp', 'bmp2.bmp' ] )
-       $ie.goto($htmlRoot + 'buttons1.html' )
-       @file_list = []       
+    
+    def setup
+        delete_captured_files( [ 'jpeg1.jpg' , 'jpeg2.jpg' , 'bmp1.bmp', 'bmp2.bmp' ] )
+        $ie.goto($htmlRoot + 'buttons1.html' )
+        @file_list = []       
     end
-
-    def teardown()
+    
+    def teardown
         delete_captured_files
     end
-
+    
     def delete_captured_files(files=nil )
         files = @file_list unless files
         files.each do |f|
             File.delete( f) if FileTest.exists?( f)
         end
     end
-
+    
     def test_jpeg
         file_name= 'jpeg1.jpg'
         @file_list << file_name
         screen_capture( file_name  )
         assert(FileTest.exist?( file_name) )
-
+        
         file_name= 'jpeg2.jpg'
         @file_list << file_name
         screen_capture( file_name , true  )   # just the active window
         assert(FileTest.exist?( file_name) )
     end
-
+    
     def test_bmp
         file_name= 'bmp1.bmp'
         @file_list << file_name
         screen_capture( file_name , false, true )
         assert(FileTest.exist?( file_name ) )
-
+        
         file_name= 'bmp2.bmp'
         @file_list << file_name
         screen_capture( file_name , true , true )   # just the active window
