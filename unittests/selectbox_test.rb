aaae4edce6f2c1caed731414b52bc48a5664811e
pmr16366 <pmr16366@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-05-25 03:12:33 +0000

    fixed some changes where i was experimenting removing the java style code for default_attribute

--- unittests/selectbox_test.rb
+++ unittests/selectbox_test.rb
@@ -59,47 +59,44 @@ class TC_Selectbox < Test::Unit::TestCase
     
 
     def test_default_attribute_for_all
-        $ie.default_attribute= :id
-        assert_equal(:id , $ie.default_attribute)
+        $ie.set_default_attribute( :id )
+        assert_equal('id' , $ie.get_default_attribute)
         assert_raises(UnknownObjectException ) { $ie.select_list('missing_id').id }
         assert_equal("o1"  , $ie.select_list('selectbox_4').value  ) 
-        $ie.default_attribute= nil 
+        $ie.set_default_attribute( nil  )
 
 
     end
 
     def test_default_attribute_for_select_list
 
-        #$ie.default_attribute_for_element= { :select_list =>  :id } 
-        SelectList.default_attribute=:id
-        #assert_equal('id' , $ie.default_attribute_for( :select_list) )
-        assert_equal(:id , SelectList.default_attribute )
-
+        $ie.set_default_attribute_for_element(:select_list ,  :id )
+        assert_equal('id' , $ie.get_default_attribute_for( :select_list) )
 
         assert_equal("o1"  , $ie.select_list('selectbox_4').value  ) 
 
-        #$ie.default_attribute_for_element= { :select_list => :name }
-        $SelectList.default_attribute=:name 
+        $ie.set_default_attribute_for_element( :select_list , :name )
 
-        assert_equal('name' , SelectList.default_attribute )
+        assert_equal('name' , $ie.get_default_attribute_for(:select_list) )
         assert_raises(UnknownObjectException ) { $ie.select_list('missing_name').value }
         assert_equal("o3"  , $ie.select_list('sel1').value) 
 
      
         # make sure that setting the default for a select_list directly, overrides the all setting
         # we are still using the name attribute, set a few lines up
-        $ie.default_attribute= :id
+        $ie.set_default_attribute( :id )
         assert_equal("o3"  , $ie.select_list('sel1').value)  #'sel1' is a name 
 
 
-        # delete the text_field type
-        SelectList.default_attribute=nil 
+        # delete the select_list type
+        $ie.set_default_attribute_for_element( :select_list , nil)
+
 
         # make sure the global attribute (id)  is used
         assert_equal("o1"  , $ie.select_list('selectbox_4').value  )   # selectbox_4 is an id
 
         # clear the global attribute
-        $ie.default_attribute= nil 
+        $ie.set_default_attribute( nil )
 
     end
 
