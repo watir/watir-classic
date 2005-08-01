8c98424348723cf937150452091b0b11b29877d7
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-01 04:24:42 +0000

    remove rdoc for deprecated interfaces

--- build-rdoc.bat
+++ build-rdoc.bat
@@ -1,4 +1,2 @@
-
-
-rdoc -m ReadMe --title "Watir API Reference" -o rdoc  --exclude "unittests|examples|install.rb" 
+rdoc -m ReadMe --title "Watir API Reference" -o rdoc --exclude "unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb" 
 
