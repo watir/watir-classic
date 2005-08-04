0f9251531ed32aa13d50e74282817b9df09f4f6b
(no author) <(no author)@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-04 06:01:11 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_4'.

--- wtr/cliec/contrib/motunes/lib/borges/lib/Borges/TestCase.rb
+++
@@ -1,4 +0,0 @@
-require 'Borges/TestCase/ContinuationTest'
-require 'Borges/TestCase/LRUCacheTest'
-require 'Borges/TestCase/StateRegistryTest'
-
--- wtr/scripting101/timeclock/timeclock/util/patches.rb
+++
@@ -1,8 +0,0 @@
-require 'drb'
-
-# In the 1.6.7 delivered with Mac OS X 1.2, Socket::TCP_NODELAY is undefined.
-# Here is the value from /usr/include/netinet/tcp.h
-
-Socket.const_set('TCP_NODELAY', 1) unless Socket.const_defined?('TCP_NODELAY')
-
-
