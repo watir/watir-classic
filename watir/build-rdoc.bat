7d3ced0e55100242cd67647ef4d38ee7b57f8807
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-19 06:17:54 +0000

    solution to paul's request that Element methods all get rdoc

--- watir/build-rdoc.bat
+++ watir/build-rdoc.bat
@@ -1,2 +1,3 @@
+# note use -Adef_wrap=R -Adef_wrap_guard=R flags to gen RDOC for those things in Elements
 rdoc -m ReadMe --title "Watir API Reference" -o rdoc --exclude "unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb" 
 
