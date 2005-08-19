f1b2c7e19b495ead66a5d2ca3d2f3da78c2b4512
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-19 06:18:29 +0000

    first step in getting lazy references

---
+++ unittests/defer_test.rb
@@ -0,0 +1,25 @@
+# tests of deferring when a Watir object is bound to a com object.
+
+# revision: $Revision$
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
+require 'unittests/setup'
+require 'unittests/textfields_test.rb'
+
+class TC_Defer < Test::Unit::TestCase
+    def teardown
+        @ie_new.close if defined?(@ie_new)
+    end
+    def test_no_page_loaded
+        @ie_new = Watir::IE.new
+        text_field = @ie_new.text_field(:name, 'text1')
+        @ie_new.goto($htmlRoot + "textfields1.html")
+        assert_equal('Hello World', text_field.value)
+    end
+    def test_refresh
+        $ie.goto($htmlRoot + "textfields1.html")
+        text_field = $ie.text_field(:name, 'text1')
+        $ie.refresh
+        assert_equal('Hello World', text_field.value)
+    end
+end
