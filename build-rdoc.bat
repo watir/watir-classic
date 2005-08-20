7066f40621156da45847c7bfdfba19d3f5b4d5f8
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-20 20:22:25 +0000

    include doc for attributes defined with def_wrap* (in Elements); change location of rdoc

--- build-rdoc.bat
+++ build-rdoc.bat
@@ -1,3 +1,5 @@
 rmdir /s /q doc\rdoc
-rdoc --title "Watir API Reference" -A def_wrap -m ReadMe -o "doc/rdoc" --exclude "unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb" 
+rem   Note: We have too many arguments to rdoc for the rdoc batch script to work correctly (it won't pass more than 9)
+rem   This is why we call ruby directly
+ruby c:\ruby\bin\rdoc -t "Watir API Reference" -A "def_wrap=R,def_wrap_guard=R" -m ReadMe -o "doc/rdoc" -x "pkg|rakefile.rb|unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb"
 
