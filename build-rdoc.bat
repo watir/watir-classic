bffc423b970f1912a80dba886b48962527e88e1a
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-20 19:55:24 +0000

    still not quite working correctly

--- build-rdoc.bat
+++ build-rdoc.bat
@@ -1,3 +1,3 @@
-# note use -Adef_wrap=R -Adef_wrap_guard=R flags to gen RDOC for those things in Elements
-rdoc -m ReadMe --title "Watir API Reference" -o doc\rdoc --exclude "unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb" 
+rmdir /s /q doc\rdoc
+rdoc --title "Watir API Reference" -A def_wrap -m ReadMe -o "doc/rdoc" --exclude "unittests|examples|install.rb|camel_case.rb|testUnitAddons.rb" 
 
