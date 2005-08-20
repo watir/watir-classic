a1dea6baf16244a3eb5db2e3f78376993a0ebc59
(no author) <(no author)@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-20 17:38:49 +0000

    This commit was manufactured by cvs2svn to create branch 'watir1_4truly'.

--- wtr/cliec/contrib/motunes/lib/borges/do_setup.bat
+++
@@ -1,3 +0,0 @@
-setup.rb config
-setup.rb setup
-setup.rb install
--- wtr/scripting101/timeclock/ruby-trace/ruby-trace/start/todo.rb
+++
@@ -1,9 +0,0 @@
-# Ruby-Trace 
-# Brian Marick, marick@visibleworkings.com, www.visibleworkings.com
-# $Revision$ ($Tag$) of $Date$
-# Copyright (C) 2001 by Brian Marick. See LICENSE.txt in the root directory.
-
-# Start with this (with ruby -r, usually) to put 'todo' messages to stdout.
-
-require 'ruby-trace/util/todo'
-Todo.enable_printing
