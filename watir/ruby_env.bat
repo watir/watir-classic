465bb76df286b02a26364244b0f63d316eb932b1
dragonkh <dragonkh@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-20 20:49:57 +0000

    *** empty log message ***

--- watir/ruby_env.bat
+++
@@ -1,5 +0,0 @@
-# This bat file is used in conjunction with the NSIS installer to determine the location of the local ruby installation
-# This file must be next to the watir_installer.exe for the installer to work.
-# Kingsley Hendrickse @ thoughtworks (20/08/2005)
-
-ruby -e ' f=File.new("ruby_env.txt", "w") ; f.puts Config::CONFIG[\'sitelibdir\'].gsub("/" , "\\") ; f.close'
