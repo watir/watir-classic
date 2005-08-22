fb4c567bdbe1e4b262926b615a0fdbf5f5d8c163
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-22 06:11:12 +0000

    gem build scripts for 1.4.1

--- watir/rakefile.rb
+++ watir/rakefile.rb
@@ -26,8 +26,8 @@ if defined? Rake::GemPackageTask
 
   Rake::GemPackageTask.new(gemspec) do |p|
     p.gem_spec = gemspec
-    p.need_tar = true
-    p.need_zip = true
+    p.need_tar = false
+    p.need_zip = false
   end
 
 else
