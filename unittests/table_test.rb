e2877652a36e8a73aa6cdd3973a7fd39acb4f96c
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-08-31 01:03:04 +0000

    white space formatting

--- unittests/table_test.rb
+++ unittests/table_test.rb
@@ -6,36 +6,35 @@ require 'unittests/setup'
 
 class TC_Tables < Test::Unit::TestCase
     include Watir
-
+    
     def setup
         gotoTablePage
     end
-
+    
     def gotoTablePage()
         $ie.goto($htmlRoot + "table1.html")
     end
-
+    
     def test_Table_Exists
-       assert_false($ie.table(:id , 'missingTable').exists? )
-       assert_false($ie.table(:index, 33).exists? )
-
-       assert($ie.table(:id, 't1').exists? )
-       assert($ie.table(:id, /t/).exists? )
-       assert_false($ie.table(:id , /missing_table/).exists? )
-
-
-       assert($ie.table(:index, 1).exists? )
-       assert($ie.table(:index, 2).exists? )
+        assert_false($ie.table(:id , 'missingTable').exists? )
+        assert_false($ie.table(:index, 33).exists? )
+        
+        assert($ie.table(:id, 't1').exists? )
+        assert($ie.table(:id, /t/).exists? )
+        assert_false($ie.table(:id , /missing_table/).exists? )
+        
+        assert($ie.table(:index, 1).exists? )
+        assert($ie.table(:index, 2).exists? )
     end
-
+    
     def test_rows
-       assert_raises( UnknownTableException  ){ $ie.table(:id , 'missingTable').row_count }
-       assert_raises( UnknownTableException  ){ $ie.table(:index , 66).row_count }
-
+        assert_raises( UnknownTableException  ){ $ie.table(:id , 'missingTable').row_count }
+        assert_raises( UnknownTableException  ){ $ie.table(:index , 66).row_count }
+        
         assert_equal( 2 , $ie.table(:index , 1).row_count)
         assert_equal( 5 , $ie.table(:id, 't1').row_count)   # 4 rows and a header 
         assert_equal( 5 , $ie.table(:index, 2).row_count)   # same table as above, just accessed by index 
-
+        
         # test the each iterator on rows - ie, go through each cell
         row = $ie.table(:index, 2)[2]
         count=1
@@ -51,106 +50,106 @@ class TC_Tables < Test::Unit::TestCase
         assert_equal(2, count -1)
         assert_equal(2, $ie.table(:index, 2)[2].column_count)        
     end
-  
+    
     def test_dynamic_tables
         t = $ie.table(:id, 't1')
         assert_equal( 5, t.row_count)
-
+        
         $ie.button(:value , 'add row').click
         assert_equal( 6, t.row_count)
     end
-
+    
     def test_columns
-
+        
         assert_raises( UnknownTableException  ){ $ie.table(:id , 'missingTable').column_count }
         assert_raises( UnknownTableException  ){ $ie.table(:index , 77).column_count }
         assert_equal( 2 , $ie.table(:index , 1).column_count)
         assert_equal( 1 , $ie.table(:id, 't1').column_count)   # row one has 1 cell with a colspan of 2
     end
-
+    
     def test_to_a
         table1Expected = [ ["Row 1 Col1" , "Row 1 Col2"] ,[ "Row 2 Col1" , "Row 2 Col2"] ]
         assert_equal(table1Expected, $ie.table(:index , 1).to_a )
     end
-
-    def test_simple_table_access
-         $ie.goto($htmlRoot + "simple_table.html")
- 
-         table = $ie.table(:index,1)
     
-         assert_equal("Row 3 Col1",table[3][1].text.strip)
-         assert_equal("Row 1 Col1",table[1][1].text.strip)
-         assert_equal("Row 3 Col2",table[3][2].text.strip)
-         assert_equal(2,table.column_count)
+    def test_simple_table_access
+        $ie.goto($htmlRoot + "simple_table.html")
+        
+        table = $ie.table(:index,1)
+        
+        assert_equal("Row 3 Col1",table[3][1].text.strip)
+        assert_equal("Row 1 Col1",table[1][1].text.strip)
+        assert_equal("Row 3 Col2",table[3][2].text.strip)
+        assert_equal(2,table.column_count)
     end
-  
+    
     def test_simple_table_buttons
         $ie.goto($htmlRoot + "simple_table_buttons.html")
- 
+        
         table = $ie.table(:index,1)
-   
+        
         table[1][1].button(:index,1).click
         assert($ie.textField(:name,"confirmtext").verify_contains(/CLICK1/i))
         table[2][1].button(:index,1).click
         assert($ie.textField(:name,"confirmtext").verify_contains(/CLICK2/i))
-
+        
         table[1][1].button(:id,'b1').click
         assert($ie.textField(:name,"confirmtext").verify_contains(/CLICK1/i))
-
+        
         assert_raises(UnknownObjectException   ) { table[1][1].button(:id,'b_missing').click }
-
+        
         table[3][1].button(:index,2).click
         assert($ie.textField(:name,"confirmtext").verify_contains(/TOO/i))
-
+        
         table[3][1].button(:value ,"Click too").click
         assert($ie.textField(:name,"confirmtext").verify_contains(/TOO/i))
         
         $ie.table(:index,1)[4][1].text_field(:index,1).set("123")
         assert($ie.text_field(:index,2).verify_contains("123"))
-
+        
         # check when a cell contains 2 objects
-
+        
         # if there were 2 different html objects in the same cell, some weird things happened ( button caption could change for example)
         assert_equal( 'Click ->' , $ie.table(:index,1)[5][1].text_field(:index,1).value )
         $ie.table(:index,1)[5][1].text_field(:index,1).click
         assert_equal( 'Click ->' , $ie.table(:index,1)[5][1].text_field(:index,1).value )
-
+        
         $ie.table(:index,1)[5][1].button(:index,1).click
         assert_equal( '' , $ie.table(:index,1)[5][1].text_field(:index,1).value )
     end
- 
+    
     def test_simple_table_gif
         $ie.goto($htmlRoot + "simple_table_buttons.html")
- 
+        
         table = $ie.table(:index,2)
-
+        
         assert_match( /1\.gif/   , table[1][1].image( :index,1).src  )
         assert_match( /2\.gif/   , table[1][2].image( :index ,1).src )
         assert_match( /3\.gif/   , table[1][3].image( :index ,1).src    )
-
+        
         assert_match( /1\.gif/   , table[3][1].image( :index ,1).src  )
         assert_match( /2\.gif/   , table[3][2].image( :index ,1).src )
         assert_match( /3\.gif/   , table[3][3].image( :index ,1).src  )
- 
+        
         table = $ie.table(:index,3)
         assert_match( /1\.gif/   , table[1][1].image( :index ,1).src  )
         assert_match( /2\.gif/   , table[1][1].image( :index ,2).src )
         assert_match( /3\.gif/   , table[1][1].image( :index ,3).src )
-
+        
         assert_match( /1\.gif/  , table[3][1].image( :index ,1).src  )
         assert_match( /2\.gif/  , table[3][1].image( :index ,2).src    )
         assert_match( /3\.gif/  , table[3][1].image( :index ,3).src  )
     end
-
+    
     def test_table_with_hidden_or_visible_rows
         $ie.goto($htmlRoot + "simple_table_buttons.html")
         t = $ie.table(:id , 'show_hide')
-
+        
         # expand the table
         t.each do |r|
             r[1].image(:src, /plus/).click if r[1].image(:src, /plus/).exists?
         end
-
+        
         # shrink rows 1,2,3
         count=1
         t.each do |r|
@@ -158,54 +157,54 @@ class TC_Tables < Test::Unit::TestCase
             count=2
         end
     end
-
+    
     def test_links_and_images_in_table
         table = $ie.table(:id, 'pic_table')
         image = table[1][2].image(:index,1)
         assert_equal("106", image.width)
-
+        
         link = table[1][4].link(:index,1)
         assert_equal("Google", link.innerText)
     end
-
+    
     def test_table_from_element
         $ie.goto($htmlRoot + "simple_table_buttons.html")
-     
+        
         button = $ie.button(:id, "b1")
         table = Table.create_from_element($ie, button)
-       
+        
         table[2][1].button(:index, 1).click
         assert($ie.textField(:name, "confirmtext").verify_contains(/CLICK2/i))
     end
-
+    
     def test_complex_table_access
         $ie.goto($htmlRoot + "complex_table.html")
-     
+        
         table = $ie.table(:index,1)
-       
+        
         assert_equal("subtable1 Row 1 Col1",table[1][1].table(:index,1)[1][1].text.strip)
         assert_equal("subtable1 Row 1 Col2",table[1][1].table(:index,1)[1][2].text.strip)
         assert_equal("subtable2 Row 1 Col2",table[2][1].table(:index,1)[1][2].text.strip)
         assert_equal("subtable2 Row 1 Col1",table[2][1].table(:index,1)[1][1].text.strip)
     end
-
+    
     def test_cell_directly
-
+        
         assert( $ie.cell(:id, 'cell1').exists? )
         assert_false( $ie.cell(:id, 'no_exist').exists? )
         assert_equal( "Row 1 Col1",  $ie.cell(:id, 'cell1').to_s.strip )
-
+        
         # not really cell directly, but just to show another way of geting the cell
         assert_equal( "Row 1 Col1",  $ie.table(:index,1)[1][1].to_s.strip )
     end
-
+    
     def test_row_directly
         assert( $ie.row(:id, 'row1').exists? )  
         assert_false( $ie.row(:id, 'no_exist').exists? )
-
+        
         assert_equal('Row 2 Col1' ,  $ie.row(:id, 'row1')[1].to_s.strip )
     end
-
+    
     def test_row_iterator
         t = $ie.table(:index,1)
         count =1
@@ -219,34 +218,34 @@ class TC_Tables < Test::Unit::TestCase
             end
         end
     end
-
+    
     def test_table_body
         assert_equal( 1, $ie.table(:index,1).bodies.length )
         assert_equal( 3, $ie.table(:id, 'body_test' ).bodies.length )
-
+        
         count = 1
         $ie.table(:id, 'body_test' ).bodies.each do |n|
-
+            
             # do something better here!
             # n.flash # this line commented out to speed up the test
-
+            
             case count 
-                when 1 
-                    compare_text = "This text is in the FRST TBODY."
-                when 2 
-                    compare_text = "This text is in the SECOND TBODY."
-                when 3 
-                    compare_text = "This text is in the THIRD TBODY."
+            when 1 
+                compare_text = "This text is in the FRST TBODY."
+            when 2 
+                compare_text = "This text is in the SECOND TBODY."
+            when 3 
+                compare_text = "This text is in the THIRD TBODY."
             end
-
+            
             assert_equal( compare_text , n[1][1].to_s.strip )   # this is the 1st cell of the first row of this particular body
-
+            
             count +=1
         end
         assert_equal( count-1, $ie.table(:id, 'body_test' ).bodies.length  )
-
+        
         assert_equal( "This text is in the THIRD TBODY." ,$ie.table(:id, 'body_test' ).body(:index,3)[1][1].to_s.strip ) 
-
+        
         # iterate through all the rows in a table body
         count = 1
         $ie.table(:id, 'body_test' ).body(:index,2).each do | row |
@@ -254,38 +253,35 @@ class TC_Tables < Test::Unit::TestCase
             if count == 1
                 assert_equal('This text is in the SECOND TBODY.' , row[1].text.strip )
             elsif count == 1
-                 assert_equal('This text is also in the SECOND TBODY.' , row[1].text.strip )
+                assert_equal('This text is also in the SECOND TBODY.' , row[1].text.strip )
             end
             count+=1
         end
     end
-
+    
     def test_get_columnvalues_single_column
         $ie.goto($htmlRoot + "simple_table_columns.html")
         assert_equal(["R1C1", "R2C1", "R3C1"], $ie.table(:index, 1).column_values(1))
     end
-
+    
     def test_colspan
         $ie.goto($htmlRoot + "simple_table_columns.html")
         assert_equal(2, $ie.table(:index, 3)[2][1].colspan)
         assert_equal(1, $ie.table(:index, 3)[1][1].colspan)
         assert_equal(3, $ie.table(:index, 3)[4][1].colspan)
-
+        
     end
-
-
-
-
+    
     def test_get_columnvalues_multiple_column
         $ie.goto($htmlRoot + "simple_table_columns.html")
         assert_equal(["R1C1", "R2C1", "R3C1"], $ie.table(:index, 2).column_values(1))
         assert_equal(["R1C3", "R2C3", "R3C3"], $ie.table(:index, 2).column_values(3))
     end
-
+    
     def test_get_columnvalues_with_colspan
         $ie.goto($htmlRoot + "simple_table_columns.html")
         assert_equal(["R1C1", "R2C1", "R3C1", "R4C1", "R5C1", "R6C2"], $ie.table(:index, 3).column_values(1))
-        (2..4).each{|x|assert_raises(UnknownCellException){$ie.table(:index, 3).column_values(x)}}
+         (2..4).each{|x|assert_raises(UnknownCellException){$ie.table(:index, 3).column_values(x)}}
     end
     
     def test_get_rowvalues_full_row
