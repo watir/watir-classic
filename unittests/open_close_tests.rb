93028bec028da08ebf766ed8abe09e80e1113ad8
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-09-14 20:39:43 +0000

    make comment

--- unittests/open_close_tests.rb
+++ unittests/open_close_tests.rb
@@ -4,6 +4,8 @@ require 'unittests/setup'
 
 class TC_OpenClose < Test::Unit::TestCase
     def setup # an extra ie window will mask the error
+    		  # it would be a more reliable test (though ruder)
+    		  # if we closed all ie windows
         $ie.close if $ie
     end
     def test_whether_win32ole_runtime_or_rpc_error_happens
