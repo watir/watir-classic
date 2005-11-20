3949c37566d8f90c2a996613fddbc858d739a52e
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2005-11-20 23:47:24 +0000

    IE.attach() now will wait for the specified window to appear, up to a timeout specified by IE.attach_timeout. The default is currently 0.2 seconds.
    Fixed numerous html format problems caught by the latest version of XML Buddy.
    
    Renamed IE#remote_eval to be IE#eval_in_spawned_process. This is accurate, even if a mouthful. We expect to wrap this in more user-friendly clothes later.

---
+++ watir/unittests/window_tests.rb
@@ -0,0 +1,10 @@
+TOPDIR = File.join(File.dirname(__FILE__), '..')
+$LOAD_PATH.unshift TOPDIR
+
+require 'unittests/setup.rb'
+
+require 'unittests/js_events_test'
+require 'unittests/modal_dialog_test'
+require 'unittests/attachToExistingWindow_test.rb'
+require 'unittests/jscript_test.rb'
+
