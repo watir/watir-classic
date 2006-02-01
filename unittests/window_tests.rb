ddf0a8990a47fa40e4c71391facb15a42910f1a2
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-02-01 05:19:34 +0000

    refactor

--- unittests/window_tests.rb
+++ unittests/window_tests.rb
@@ -3,10 +3,5 @@ $LOAD_PATH.unshift TOPDIR
 
 require 'unittests/setup.rb'
 
-require 'unittests/js_events_test'
-require 'unittests/modal_dialog_test'
-require 'unittests/attachToExistingWindow_test.rb'
-require 'unittests/attach_to_new_window_test.rb'
-require 'unittests/jscript_test.rb'
-require 'unittests/send_keys_test.rb'
-
+Dir.chdir TOPDIR
+$window_tests.each {|x| require x}
