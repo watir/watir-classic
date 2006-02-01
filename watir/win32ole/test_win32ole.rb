de6a2bed0411f7b4c48a2b89ddfbb6b1e16bf698
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-02-01 04:25:31 +0000

    make the test work

--- watir/win32ole/test_win32ole.rb
+++ watir/win32ole/test_win32ole.rb
@@ -1,29 +1,28 @@
 require 'Win32API'
 require 'win32ole'
  
-#test, make sure we can connect to the IEDialog.dll
 # this will find the IEDialog.dll file in its build location
-iedialog_file = (File.expand_path(File.dirname(__FILE__)) + "/../IEDialog/Release/IEDialog.dll").gsub('/', '\\')
+iedialog_file = (File.expand_path(File.dirname(__FILE__)) + "/../watir/IEDialog/Release/IEDialog.dll").gsub('/', '\\')
+
+# make sure we can connect to the IEDialog.dll
 fnShowString = Win32API.new(iedialog_file, 'ShowString', ['p'], 'v')
 fnShowString.call("from ruby!") 
-
 
 ie = WIN32OLE.new('InternetExplorer.Application')
 ie.visible = true
  
-#my home page is google
+# assumes home page is google
 ie.gohome
- 
 webbrowser = ie.Application
  
-#wait for google page to be displayed
+# wait for google page to be displayed
 puts webbrowser.Busy
 sleep(5)
 puts webbrowser.Busy
 
 fnGetUnknown = Win32API.new(iedialog_file, 'GetUnknown', ['p', 'p'], 'v')
 
-intPointer = " " * 4	#will contain the int value of the IUnknown*
+intPointer = " " * 4 # will contain the int value of the IUnknown*
 fnGetUnknown.call("Google - Microsoft Internet Explorer", intPointer)
 
 intArray = intPointer.unpack('L')
@@ -36,11 +35,9 @@ puts htmlDoc
 scriptEngine = htmlDoc.Script
 puts scriptEngine
 
-#now we get the HTML DOM object!
+# now we get the HTML DOM object!
 doc2 = scriptEngine.document
 puts doc2
 
 body = doc2.body
 puts body.innerHTML
-
-
