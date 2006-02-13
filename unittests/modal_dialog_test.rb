582bd61c73f73b39715d13d620418173c12b1d70
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-02-13 07:13:08 +0000

    Define IE.attach_modal. This is planned to be merged into IE.attach...

--- unittests/modal_dialog_test.rb
+++ unittests/modal_dialog_test.rb
@@ -18,7 +18,7 @@ class TC_ModalDialog < Test::Unit::TestCase
   
   def test_modal_use_case
     $ie.button(:value, 'Launch Dialog').click_no_wait
-    modal = attach_modal('Modal Dialog -- Web Page Dialog')
+    modal = IE.attach_modal('Modal Dialog -- Web Page Dialog')
 
     assert(modal.text.include?('Enter some text:'))
     modal.text_field(:name, 'modal_text').set('hello')
@@ -26,35 +26,6 @@ class TC_ModalDialog < Test::Unit::TestCase
     assert_equal('hello', $ie.text_field(:name, 'modaloutput').value)
   end
 
-  # this will find the IEDialog.dll file in its build location
-  @@iedialog_file = (File.expand_path(File.dirname(__FILE__)) + "/../watir/IEDialog/Release/IEDialog.dll").gsub('/', '\\')
-  @@fnFindWindowEx = Win32API.new('user32.dll', 'FindWindowEx', ['l', 'l', 'p', 'p'], 'l')
-  @@fnGetUnknown = Win32API.new(@@iedialog_file, 'GetUnknown', ['l', 'p'], 'v')
-
-  def attach_modal(title)
-    hwnd_modal = 0
-    until_with_timeout(10) do
-      hwnd_modal = @@fnFindWindowEx.call(0, 0, nil, title)
-      hwnd_modal > 0
-    end
-     
-    intPointer = " " * 4 # will contain the int value of the IUnknown*
-    @@fnGetUnknown.call(hwnd_modal, intPointer)
-    
-    intArray = intPointer.unpack('L')
-    intUnknown = intArray.first
-    assert(intUnknown > 0)
-      
-    htmlDoc = WIN32OLE.connect_unknown(intUnknown)    
-    ModalPage.new(htmlDoc)
-  end
-
-  def until_with_timeout(timeout) # block
-    start_time = Time.now
-    until yield or Time.now - start_time > timeout do
-      sleep 0.05
-    end
-  end
 end
 
 class ModalPage < Watir::IE
