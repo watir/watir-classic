85555c019838ac0c3a1277f8198c99b99bbdc5f7
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-05-02 00:16:03 +0000

    added IE.exists? which returns whether the browser still exists. removed Watir::exists? which never worked right anyway.

---
+++ unittests/ie_exists_test.rb
@@ -0,0 +1,32 @@
+# test IE#exists?
+# revision: $Revision: 962 $
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
+require 'unittests/setup'
+
+class TC_IE_Exists < Test::Unit::TestCase 
+  def setup
+    @ie = Watir::IE.new
+  end
+  def teardown
+    @ie.close
+  end
+  def test_exists
+    assert(@ie.exists?)
+  end
+end
+
+class TC_IENotExists < Test::Unit::TestCase 
+  def setup
+    @ie = Watir::IE.new
+  end
+  def test_we_closed_it
+    @ie.close
+    assert(!@ie.exists?)
+  end
+  def test_some_one_else_closed_it
+    @ie.ie.quit
+    sleep 0.3 # give it some time to close
+    assert(!@ie.exists?)
+  end    
+end
