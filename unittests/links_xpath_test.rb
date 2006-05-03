b0d5b471281f8cd76ded7c9751943798fb4148a9
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-05-03 04:01:47 +0000

    test cleanup

--- unittests/links_xpath_test.rb
+++ unittests/links_xpath_test.rb
@@ -7,46 +7,33 @@ require 'unittests/setup'
 class TC_Links_XPath < Test::Unit::TestCase
   include Watir
   
-  def gotoLinksPage
+  def setup
     $ie.goto($htmlRoot + "links1.html")
   end
   
-  def setup
-    gotoLinksPage
-  end
-  
-  def xtest_new_link_exists
-    assert(exists?{$ie.link(:xpath , "//a[contains(.,'test1')]/")})
-    assert(exists?{$ie.link(:xpath, "//a[contains(., /TEST/i)]/")})   
-    assert(!exists?{$ie.link(:xpath , "//a[contains(.,'missing')]/")})
-    assert(!exists?{$ie.link(:xpath, "//a[contains(., /miss/)]/")})   
-    
-  end
-  
-  def test_Link_Exists
-    assert($ie.link(:xpath , "//a[contains(.,'test1')]/").exists?)
+  def test_link_exists
+    assert($ie.link(:xpath, "//a[contains(.,'test1')]/").exists?)
     assert($ie.link(:xpath, "//a[contains(., /TEST/i)]/").exists?)   
-    assert(!$ie.link(:xpath , "//a[contains(.,'missing')]/").exists?)
+    assert(!$ie.link(:xpath, "//a[contains(.,'missing')]/").exists?)
     
-    assert(!$ie.link(:xpath , "//a[@url='alsomissing.html']/").exists?)
+    assert(!$ie.link(:xpath, "//a[@url='alsomissing.html']/").exists?)
     
-    assert($ie.link(:xpath , "//a[@id='link_id']/").exists?)
-    assert(!$ie.link(:xpath , "//a[@id='alsomissing']/").exists?)
+    assert($ie.link(:xpath, "//a[@id='link_id']/").exists?)
+    assert(!$ie.link(:xpath, "//a[@id='alsomissing']/").exists?)
     
-    assert($ie.link(:xpath , "//a[@name='link_name']/").exists?)
-    assert(!$ie.link(:xpath , "//a[@name='alsomissing']/").exists?)
-    assert($ie.link(:xpath , "//a[@title='link_title']/").exists?)
-    
+    assert($ie.link(:xpath, "//a[@name='link_name']/").exists?)
+    assert(!$ie.link(:xpath, "//a[@name='alsomissing']/").exists?)
+    assert($ie.link(:xpath, "//a[@title='link_title']/").exists?)
   end
   
-  def test_Link_click
-    $ie.link(:xpath , "//a[contains(.,'test1')]/").click
-    assert( $ie.text.include?("Links2-Pass") )
+  def test_link_click
+    $ie.link(:xpath, "//a[contains(.,'test1')]/").click
+    assert($ie.text.include?("Links2-Pass"))
   end
   
   def test_link_with_text_call
-    $ie.link(:xpath , "//a[text()='test1']").click
-    assert( $ie.text.include?("Links2-Pass") )
+    $ie.link(:xpath, "//a[text()='test1']").click
+    assert($ie.text.include?("Links2-Pass"))
   end
   
 end
