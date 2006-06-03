b263484e8593505931061e5151e26bc66f1b2456
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-06-03 06:52:05 +0000

    added ie.close_modal and ie.close_others

--- watir/close_all.rb
+++ watir/close_all.rb
@@ -2,13 +2,37 @@ require 'watir'
 
 module Watir
   class IE
+    # close all ie browser windows
     def self.close_all
+      close_all_but nil
+    end
+    # find other ie browser windows and close them
+    def close_others
+      IE.close_all_but self
+    end
+    private
+    def self.close_all_but(except=nil)
       shell = WIN32OLE.new 'Shell.Application'
       shell.windows.each do |window|
         next unless window.path =~ /Internet Explorer/ 
-        IE.bind(window).close    
+        ie = IE.bind window
+        ie.close_modal
+        ie.close unless except && except.hwnd == window.hwnd
       end
       sleep 1.0 # replace with polling for window count to be zero?
+    end
+    public
+    # close modal dialog. unlike IE#modal_dialog.close, does not wait for dialog
+    # to appear and does not raise exception if no window is found.
+    def close_modal
+      begin
+        original_attach_timeout = IE.attach_timeout
+        IE.attach_timeout = 0
+        self.modal_dialog.close
+      rescue NoMatchingWindowFoundException
+      ensure
+        IE.attach_timeout = original_attach_timeout
+      end
     end
   end
 end
