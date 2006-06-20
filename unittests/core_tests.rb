77f4b044106b18cab312762f303e089f7c58e7b6
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2006-06-20 17:07:38 +0000

    small cleanup for core tests. ie window is no longer visible for just a moment.

--- unittests/core_tests.rb
+++ unittests/core_tests.rb
@@ -3,10 +3,11 @@
 TOPDIR = File.join(File.dirname(__FILE__), '..')
 $LOAD_PATH.unshift TOPDIR
 
+require 'watir'
+
+$HIDE_IE = true
+
 require 'unittests/setup.rb'
 
 Dir.chdir TOPDIR
 $core_tests.each {|x| require x}
-
-$HIDE_IE = true
-$ie.visible = false
