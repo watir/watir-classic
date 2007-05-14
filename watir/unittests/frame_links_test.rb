0ca914c2d2f1df8c30d43aa4454a3e443529129b
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-05-14 22:50:13 +0000

    Update close_window_test.rb & frame_links_test.rb so that they work with IE7. Update watir/WindowHelper so that it works with IE7. Minor code cleanup to jscript_test.rb.

--- watir/unittests/frame_links_test.rb
+++ watir/unittests/frame_links_test.rb
@@ -18,7 +18,7 @@ class TC_Frame_Links < Test::Unit::TestCase
   
   def test_click_no_wait_in_a_frame
     $ie.frame('linkFrame').link(:text, 'test1').click_no_wait
-    wait_until(1){$ie.frame('linkFrame').text.include?('Links2-Pass')}
+    wait_until(2){$ie.frame('linkFrame').text.include?('Links2-Pass')}
     assert($ie.frame('linkFrame').text.include?('Links2-Pass'))
   end  
   
