4674eb976d66b6d955c6746f69e3c7503ddda84d
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-05-15 21:44:46 +0000

    Update open-close test so that it regularly (usually 2-4 times) fails when run. This seems to be quite timing dependent and may have to be tweaked to get similar results on other machines.

--- unittests/open_close_tests.rb
+++ unittests/open_close_tests.rb
@@ -1,23 +1,14 @@
 # rapidly open and close IE windows
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
-require 'unittests/setup'
+require 'test/unit'
+require 'watir'
 
 class TC_OpenClose < Test::Unit::TestCase
-  def setup # an extra ie window will mask the error
-    # it would be a more reliable test (though ruder)
-    # if we closed all ie windows
-    $ie.close if $ie
-  end
-  def test_whether_win32ole_runtime_or_rpc_error_happens
-    20.times do
-      assert_nothing_raised do
-        ie = Watir::IE.new
-        ie.close
-      end
+  20.times do | i |
+    define_method "test_#{i}" do
+      sleep 0.05
+      sleep i * 0.01
+      ie = Watir::IE.start 'http://blogs.dovetailsoftware.com/blogs/gsherman/default.aspx'
+      ie.close
     end
-  end
-  def teardown
-    @ie.close if defined?(@ie)
-    start_ie_with_logger
   end
 end       
