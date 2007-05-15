8bb42bc809a74792a6fd54dbe6368ffd60209e97
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-05-15 15:29:15 +0000

    Update dialog.rb for IE7.

--- watir/dialog.rb
+++ watir/dialog.rb
@@ -4,18 +4,19 @@ require 'watir'
 module Watir
 
     class Dialog
+        WindowName = 'Windows Internet Explorer'    
         def button(name)
             DialogButton.new(name)
         end
         def close
             # TODO: register autoit before use
             autoit = WIN32OLE.new('AutoItX3.Control')
-            autoit.WinClose "Microsoft Internet Explorer", ""
+            autoit.WinClose WindowName, ""
         end
         def exists?
             # TODO: register autoit before use
             autoit = WIN32OLE.new('AutoItX3.Control')
-            found = autoit.WinWait("Microsoft Internet Explorer", "", 1)
+            found = autoit.WinWait(WindowName, "", 1)
             return found == 1
         end 
     end
@@ -31,9 +32,9 @@ module Watir
         def click
             # TODO: register autoit before use
             autoit = WIN32OLE.new('AutoItX3.Control')
-            autoit.WinWait "Microsoft Internet Explorer", "", 1
+            autoit.WinWait Dialog::WindowName, "", 1
             name_pattern = Regexp.new "^#{@name}$"
-            unless name_pattern =~ autoit.WinGetText("Microsoft Internet Explorer", "")
+            unless name_pattern =~ autoit.WinGetText(Dialog::WindowName, "")
                 raise Watir::Exception::UnknownObjectException
             end
             autoit.Send "{ENTER}"
