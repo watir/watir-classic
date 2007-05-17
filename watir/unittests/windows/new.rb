3627eaf97562541207fda3a240763fe61e8ed57b
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-05-17 20:41:40 +0000

    Add Watir::IE.start_process Fix require of win32-process (it was broken, but masked by a bug in rubygems) Add win32-process as a dependency of the watir gem.

--- watir/unittests/windows/new.rb
+++ watir/unittests/windows/new.rb
@@ -1,7 +1,8 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..') if $0 == __FILE__
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '../..') if $0 == __FILE__
+
 require 'unittests/setup'
-require 'watir/process'
 require 'watir/contrib/ie-new-process'
+require 'watir/process'
 
 class TC_New < Test::Unit::TestCase
   include Watir::Process
@@ -18,11 +19,11 @@ class TC_New < Test::Unit::TestCase
       @background_iexplore_process = 1
     end
   end
-  
+
   def teardown
     @background.close if @background
     @new.close if @new
-    sleep 0.5 # give it time to close
+    sleep 1.0 # give it time to close
   end
 
   def test_new_window_does_not_create_new_process
@@ -44,6 +45,12 @@ class TC_New < Test::Unit::TestCase
   def test_new_process_creates_a_new_process
     @new = Watir::IE.new_process
     assert_equal @background_iexplore_processes + 1, ie_process_count
+  end
+  
+  def test_start_process_with_arg_creates_a_new_process_and_goes_to_that_page
+    @new = Watir::IE.start_process 'http://wtr.rubyforge.org'
+    assert_equal @background_iexplore_processes + 1, ie_process_count
+    assert_equal 'http://wtr.rubyforge.org/', @new.url
   end
 end
      
