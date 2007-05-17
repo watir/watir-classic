135358ebb6bae0e4288e6313fdd434de8381df87
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-05-17 16:02:45 +0000

    reformat

--- watir/watir/process.rb
+++ watir/watir/process.rb
@@ -1,12 +1,13 @@
 module Watir
   module Process
-
-  # Returns the number of windows processes running with the specified name.
-  def count_processes name
-    mgmt = WIN32OLE.connect('winmgmts:\\\\.')
-    processes = mgmt.InstancesOf('win32_process')
-    processes.extend Enumerable
-    processes.select{|x| x.name == name}.length
+    
+    # Returns the number of windows processes running with the specified name.
+    def count_processes name
+      mgmt = WIN32OLE.connect('winmgmts:\\\\.')
+      processes = mgmt.InstancesOf('win32_process')
+      processes.extend Enumerable
+      processes.select{|x| x.name == name}.length
+    end
+    
   end
-  
-end; end
+end
