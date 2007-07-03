48d2db5070be0aa8ea10f49ca82a170d5329f03b
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-07-03 15:08:31 +0000

    Add support for h4, h5 and h6.

--- watir/elements.rb
+++ watir/elements.rb
@@ -2,7 +2,6 @@ module Watir
   class Ul < NonControlElement
     TAG = 'UL'
   end
-  
   module Container
     def ul(how, what)
       return Ul.new(self, how, what)
@@ -12,7 +11,6 @@ module Watir
   class H1 < NonControlElement
     TAG = 'H1'
   end
-  
   module Container
     def h1(how, what)
       return H1.new(self, how, what)
@@ -22,7 +20,6 @@ module Watir
   class H2 < NonControlElement
     TAG = 'H2'
   end
-  
   module Container
     def h2(how, what)
       return H2.new(self, how, what)
@@ -32,10 +29,36 @@ module Watir
   class H3 < NonControlElement
     TAG = 'H3'
   end
-
   module Container
     def h3(how, what)
       return H3.new(self, how, what)
+    end
+  end
+
+  class H4 < NonControlElement
+    TAG = 'H4'
+  end
+  module Container
+    def h4(how, what)
+      return H4.new(self, how, what)
+    end
+  end
+
+  class H5 < NonControlElement
+    TAG = 'H5'
+  end
+  module Container
+    def h5(how, what)
+      return H5.new(self, how, what)
+    end
+  end
+  class H6 < NonControlElement
+    TAG = 'H6'
+  end
+
+  module Container
+    def h6(how, what)
+      return H6.new(self, how, what)
     end
   end
 
