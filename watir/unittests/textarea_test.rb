cd22bb881731597c6a70a65baafa2ac9872a01df
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-10-03 03:50:15 +0000

    Test cleanup - reorganize tests, and provide descriptive names - remove redundant tests - reformat for readability

--- watir/unittests/textarea_test.rb
+++ watir/unittests/textarea_test.rb
@@ -1,4 +1,4 @@
-# feature tests for TextArea Fields
+# tests for textarea elements
 # revision: $Revision$
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
@@ -7,13 +7,15 @@ require 'unittests/setup'
 class TC_TextArea < Test::Unit::TestCase
   include Watir
   
-  def goto_page
-    $ie.goto($htmlRoot + "textArea.html")
+  def use_page page
+    $ie.goto($htmlRoot + page)
+  end
+
+  def setup
+    use_page 'textarea.html'
   end
   
-  def test_textarea_field_exists
-    goto_page
-    #test for existance of 4 text area
+  def test_elements_exist_or_not
     assert($ie.text_field(:name,"txtMultiLine1").exists?)
     assert($ie.text_field(:name,"txtMultiLine2").exists?)
     assert($ie.text_field(:name,"txtMultiLine3").exists?)
@@ -23,65 +25,73 @@ class TC_TextArea < Test::Unit::TestCase
     assert($ie.text_field(:id,"txtMultiLine2").exists?)
     assert($ie.text_field(:id,"txtMultiLine3").exists?)
     assert($ie.text_field(:id,"txtReadOnly").exists?)
-    #test for missing 
+
     assert(!$ie.text_field(:name, "missing").exists?)   
     assert(!$ie.text_field(:name,"txtMultiLine4").exists?)
   end
   
-  def test_textarea_to_s
+  def test_to_s_bug
     # from a bug reported by Zeljko Filipin
-    goto_page
     assert_nothing_raised { $ie.text_field(:id,"txtMultiLine3").to_s  }
   end
   
-  def test_textarea_maxlength
-    goto_page
+  def test_maxlength_bug
+    # from another bug
     assert_nothing_raised { $ie.text_field(:id,"txtMultiLine3").append('foo')} 
   end
   
-  def test_textarea_field
-    goto_page
-    
-    # test for read only method
+  def test_readonly_and_enabled
     assert(!$ie.text_field(:name, "txtMultiLine1").readonly? )  
     assert($ie.text_field(:name,"txtReadOnly").readonly?)
     
-    # test for enabled? method
     assert(!$ie.text_field(:name, "txtDisabled").enabled? )  
     assert($ie.text_field(:id, "txtMultiLine1").enabled? )  
-    
-    
+  end
+  
+  def test_verify_contains
     t1 = $ie.text_field(:name, "txtMultiLine1")
     assert(t1.verify_contains("Hello World") )  
     assert(t1.verify_contains(/el/) )  
-    t2 = $ie.text_field(:name, "txtMultiLine2")
-    assert(t2.verify_contains(/IE/))
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.text_field(:name, "NoName").verify_contains("No field to get a value of") }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.text_field(:id, "noID").verify_contains("No field to get a value of") }  
-    
-    assert_raises(UnknownObjectException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:name, "txtNone").append("Some Text") }  
-    
-    assert_raises(ObjectReadOnlyException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:id, "txtReadOnly").append("Some Text") }  
-    assert_raises(ObjectDisabledException   , "ObjectDisabledException   was supposed to be thrown" ) {   $ie.text_field(:name, "txtDisabled").append("Some Text") }  
-    assert_raises(UnknownObjectException  , "UnknownObjectException  was supposed to be thrown" ) {   $ie.text_field(:name, "missing_field").append("Some Text") }  
-    
+    assert($ie.text_field(:name, "txtMultiLine2").verify_contains(/IE/))
+  end
+  
+  def test_no_such_element
+    assert_raises(UnknownObjectException) do
+      $ie.text_field(:name, "NoName").verify_contains("de nada")
+    end  
+    assert_raises(UnknownObjectException) do
+      $ie.text_field(:id, "noID").verify_contains("de nada")
+    end
+    assert_raises(UnknownObjectException) do
+      $ie.text_field(:name, "txtNone").append("de nada")
+    end  
+  end
+  def test_readonly_and_disabled_errors
+    assert_raises(ObjectReadOnlyException) do
+      $ie.text_field(:id, "txtReadOnly").append("de nada")
+    end  
+    assert_raises(ObjectDisabledException) do
+      $ie.text_field(:name, "txtDisabled").append("de nada")
+    end
+    assert_raises(ObjectReadOnlyException) do
+      $ie.text_field(:id, "txtReadOnly").append("Some Text")
+    end
+    assert_raises(ObjectDisabledException) do
+      $ie.text_field(:name, "txtDisabled").append("Some Text")
+    end
+  end
+
+  def test_append_set_and_clear
     $ie.text_field(:name, "txtMultiLine1").append(" Some Text")
-    assert_equal(  "Hello World Some Text" , $ie.text_field(:name, "txtMultiLine1").getContents )  
-    
-    assert_raises(ObjectReadOnlyException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:id, "txtReadOnly").append("Some Text") }  
-    assert_raises(ObjectDisabledException   , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:name, "txtDisabled").append("Some Text") }  
-    assert_raises(UnknownObjectException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:name, "missing_field").append("Some Text") }  
+    assert_equal("Hello World Some Text", 
+      $ie.text_field(:name, "txtMultiLine1").getContents )  
     
     $ie.text_field(:name, "txtMultiLine1").set("watir IE Controller")
-    assert_equal(  "watir IE Controller" , $ie.text_field(:name, "txtMultiLine1").getContents )  
-    
-    assert_raises(ObjectReadOnlyException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:id, "txtReadOnly").append("Some Text") }  
-    assert_raises(ObjectDisabledException   , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:name, "txtDisabled").append("Some Text") }  
-    assert_raises(UnknownObjectException  , "ObjectReadOnlyException   was supposed to be thrown" ) {   $ie.text_field(:name, "missing_field").append("Some Text") }  
+    assert_equal("watir IE Controller", 
+      $ie.text_field(:name, "txtMultiLine1").getContents )  
     
     $ie.text_field(:name, "txtMultiLine2").clear
-    assert_equal(  "" , $ie.text_field(:name, "txtMultiLine2").getContents )  
-    
+    assert_equal("" , $ie.text_field(:name, "txtMultiLine2").getContents )  
   end
   
 end
