329c3b996024786eec792e9cdeb208f35584939c
zeljko.filipin@gmail.com <zeljko.filipin@gmail.com@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-10-05 11:16:09 +0000

    updating Example Test Case removed unnecessary indentation

--- watir/examples/googleSearch.rb
+++ watir/examples/googleSearch.rb
@@ -12,42 +12,42 @@
 #
 #------------------------------------------------------------------------------------------------------------#
 
-   require 'watir'   # the watir controller
+require 'watir'   # the watir controller
 
-   # set a variable
-   test_site = 'http://www.google.com'
-  
-   # open the IE browser
-   ie = Watir::IE.new
+# set a variable
+test_site = 'http://www.google.com'
 
-   # print some comments
-   puts "## Beginning of test: Google search"
-   puts "  "
-  
-   puts "Step 1: go to the test site: " + test_site
-   ie.goto(test_site)
-   puts "  Action: entered " + test_site + " in the address bar."
+# open the IE browser
+ie = Watir::IE.new
 
-   puts "Step 2: enter 'pickaxe' in the search text field"
-   ie.text_field(:name, "q").set("pickaxe")       # q is the name of the search field
-   puts "  Action: entered pickaxe in the search field"
+# print some comments
+puts "## Beginning of test: Google search"
+puts "  "
 
-   puts "Step 3: click the 'Google Search' button"
-   ie.button(:name, "btnG").click   # "btnG" is the name of the Search button
-   puts "  Action: clicked the Google Search button."
+puts "Step 1: go to the test site: " + test_site
+ie.goto(test_site)
+puts "  Action: entered " + test_site + " in the address bar."
 
-   puts "Expected Result: "
-   puts " - a Google page with results should be shown. 'Programming Ruby' should be high on the list."
-  
-   puts "Actual Result: Check that the 'Programming Ruby' link appears on the results page "
-   if ie.text.include?("Programming Ruby")  
-      puts "Test Passed. Found the test string: 'Programming Ruby'. Actual Results match Expected Results."
-   else
-      puts "Test Failed! Could not find: 'Programming Ruby'" 
-   end
-   
-   puts "  "
-   puts "## End of test: Google search"
+puts "Step 2: enter 'pickaxe' in the search text field"
+ie.text_field(:name, "q").set("pickaxe")       # q is the name of the search field
+puts "  Action: entered pickaxe in the search field"
+
+puts "Step 3: click the 'Google Search' button"
+ie.button(:name, "btnG").click   # "btnG" is the name of the Search button
+puts "  Action: clicked the Google Search button."
+
+puts "Expected Result: "
+puts " - a Google page with results should be shown. 'Programming Ruby' should be high on the list."
+
+puts "Actual Result: Check that the 'Programming Ruby' link appears on the results page "
+if ie.text.include?("Programming Ruby")  
+  puts "Test Passed. Found the test string: 'Programming Ruby'. Actual Results match Expected Results."
+else
+  puts "Test Failed! Could not find: 'Programming Ruby'" 
+end
+ 
+puts "  "
+puts "## End of test: Google search"
   
 
 # -end of simple Google search test
