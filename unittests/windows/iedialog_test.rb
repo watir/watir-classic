eacfcceaa9794d009a85f5acffcc96649ce609c8
charley <charley@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-10-13 03:48:17 +0000

    Fixing path to iedialog.dll

--- unittests/windows/iedialog_test.rb
+++ unittests/windows/iedialog_test.rb
@@ -10,7 +10,7 @@ class TC_IEDialog < Test::Unit::TestCase
   include Watir 
 
   # this will find the IEDialog.dll file in its build location
-  @@iedialog_file = (File.expand_path(File.dirname(__FILE__)) + "/../watir/IEDialog/Release/IEDialog.dll").gsub('/', '\\')
+  @@iedialog_file = (File.expand_path(File.dirname(__FILE__)) + "/../../watir/IEDialog/Release/IEDialog.dll").gsub('/', '\\')
   
   # commented out because it currently requires a manual click
   # a better idea would be to automate the click...
