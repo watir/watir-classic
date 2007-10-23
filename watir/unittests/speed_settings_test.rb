199885663ffa5b7d611c64a41c644c445f43a46f
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2007-10-23 23:38:39 +0000

    New method for setting defaults: Watir::IE.defaults = {:speed => :fast, :visible => false}.

--- watir/unittests/speed_settings_test.rb
+++ watir/unittests/speed_settings_test.rb
@@ -4,19 +4,53 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
 require 'test/unit'
 require 'watir'
 
-class TC_Defaults < Test::Unit::TestCase
+class TC_instance_options < Test::Unit::TestCase
   include Watir
   
   def test_using_default
-    @ie1 = IE.new
+    @ie1 = Watir::IE.new
     @ie1.speed = :fast
     assert_equal(:fast, @ie1.speed)
     @ie1.speed = :slow
     assert_equal(:slow, @ie1.speed)
     assert_raise(ArgumentError){@ie1.speed = :fubar}
-  end    
-  
+  end
+
   def teardown
     @ie1.close if @ie1
   end
+end
+
+class TC_class_options < Test::Unit::TestCase
+	include Watir
+	@@hide_ie = $HIDE_IE
+	def setup
+		@previous = Watir::IE.defaults
+	end
+	def test_class_defaults
+		assert_equal({:speed => :slow, :visible => ! @@hide_ie}, IE.defaults)
+	end
+	def test_change_defaults
+		IE.defaults = {:speed => :fast}
+		assert_equal(:fast, IE.speed)
+		IE.defaults = {:visible => false}
+		assert_equal(false, IE.visible)
+		IE.defaults = {:speed => :slow}
+		assert_equal(:slow, IE.speed)
+		IE.defaults = {:visible => true}
+		assert_equal(true, IE.visible)
+	end
+	def test_defaults_affect_on_instance
+		IE.defaults = {:speed => :fast}
+		@ie1 = IE.new
+		assert_equal(:fast, @ie1.speed)
+		IE.defaults = {:speed => :slow}
+		@ie2 = IE.new
+		assert_equal(:slow, @ie2.speed)
+	end
+	def teardown
+		IE.defaults = @previous
+		@ie1.close if @ie1
+		@ie2.close if @ie2
+	end
 end
