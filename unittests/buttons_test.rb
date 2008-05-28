a56a12633f74cbcc4da860b24b2471c45b95b688
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-05-28 19:10:44 +0000

    replace references to $ie with browser.

--- unittests/buttons_test.rb
+++ unittests/buttons_test.rb
@@ -10,21 +10,6 @@ class TC_Buttons < Test::Unit::TestCase
     use_page "buttons1.html"
   end
   
-  def goto_frames_page
-    use_page "frame_buttons.html"
-  end
-  
-  def aatest_Spinner
-    s = Spinner.new
-    i = 0
-    while(i < 100)
-      sleep 0.05
-      print s.next
-      i+=1
-    end
-    s = nil
-  end
-  
   def aaatest_Button_to_s
     # i think the tests for to_s should be dropped. The output is not in a nice format to be tested, and the
     # individual properties are tested in the test_properties method
@@ -40,93 +25,89 @@ class TC_Buttons < Test::Unit::TestCase
         'value             Click Me',
         'disabled          false']
     
-    assert_equal(b4, $ie.button(:name, "b4").to_s)
-    assert_equal(b1, $ie.button(:caption, "Click Me").to_s)
-    assert_equal(b1, $ie.button(:index, 1).to_s)
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").to_s   }  
+    assert_equal(b4, browser.button(:name, "b4").to_s)
+    assert_equal(b1, browser.button(:caption, "Click Me").to_s)
+    assert_equal(b1, browser.button(:index, 1).to_s)
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").to_s   }  
   end
   
   def test_properties
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").id   }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").name   }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").disabled   }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").type   }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").value   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").id   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").name   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").disabled   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").type   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").value   }  
     
-    assert_equal("b1"  , $ie.button(:index, 1).name ) 
-    assert_equal("b2"  , $ie.button(:index, 1).id ) 
-    assert_equal("button"  , $ie.button(:index, 1).type  ) 
-    assert_equal("Click Me"  , $ie.button(:index, 1).value  ) 
-    assert_equal(false  , $ie.button(:index, 1).disabled  ) 
-    assert_equal("italic_button"  , $ie.button(:name, "b1").class_name  ) 
-    assert_equal(""  , $ie.button(:name , "b4").class_name  ) 
+    assert_equal("b1"  , browser.button(:index, 1).name ) 
+    assert_equal("b2"  , browser.button(:index, 1).id ) 
+    assert_equal("button"  , browser.button(:index, 1).type  ) 
+    assert_equal("Click Me"  , browser.button(:index, 1).value  ) 
+    assert_equal(false  , browser.button(:index, 1).disabled  ) 
+    assert_equal("italic_button"  , browser.button(:name, "b1").class_name  ) 
+    assert_equal(""  , browser.button(:name , "b4").class_name  ) 
+        
+    assert_equal("b1"  , browser.button(:id, "b2").name  ) 
+    assert_equal("b2"  , browser.button(:id, "b2").id  ) 
+    assert_equal("button"  , browser.button(:id, "b2").type  ) 
     
+    assert_equal("b4"  , browser.button(:index, 2).name  ) 
+    assert_equal("b5"  , browser.button(:index, 2).id  ) 
+    assert_equal("button"  , browser.button(:index, 2).type  ) 
+    assert_equal("Disabled Button"  , browser.button(:index, 2).value  ) 
+    assert_equal(true  , browser.button(:index, 2).disabled  ) 
     
-    assert_equal("b1"  , $ie.button(:id, "b2").name  ) 
-    assert_equal("b2"  , $ie.button(:id, "b2").id  ) 
-    assert_equal("button"  , $ie.button(:id, "b2").type  ) 
-    
-    assert_equal("b4"  , $ie.button(:index, 2).name  ) 
-    assert_equal("b5"  , $ie.button(:index, 2).id  ) 
-    assert_equal("button"  , $ie.button(:index, 2).type  ) 
-    assert_equal("Disabled Button"  , $ie.button(:index, 2).value  ) 
-    assert_equal(true  , $ie.button(:index, 2).disabled  ) 
-    
-    assert_equal( "" , $ie.button(:index, 2).title )
-    assert_equal( "this is button1" , $ie.button(:index, 1).title )
+    assert_equal( "" , browser.button(:index, 2).title )
+    assert_equal( "this is button1" , browser.button(:index, 1).title )
   end
-  
   
   def test_button_using_default
     # since most of the time, a button will be accessed based on its caption, there is a default way of accessing it....
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button( "Missing Caption").click   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button( "Missing Caption").click   }  
     
-    $ie.button("Click Me").click
-    assert($ie.text.include?("PASS") )
+    browser.button("Click Me").click
+    assert(browser.text.include?("PASS") )
   end
   
   def test_Button_click_only
-    $ie.button(:caption, "Click Me").click
-    assert($ie.text.include?("PASS") )
+    browser.button(:caption, "Click Me").click
+    assert(browser.text.include?("PASS") )
   end
   
   def test_button_click
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:caption, "Missing Caption").click   }  
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:id, "missingID").click   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:caption, "Missing Caption").click   }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:id, "missingID").click   }  
+    assert_raises(ObjectDisabledException , "ObjectDisabledException was supposed to be thrown" ) {   browser.button(:caption, "Disabled Button").click   }  
     
-    assert_raises(ObjectDisabledException , "ObjectDisabledException was supposed to be thrown" ) {   $ie.button(:caption, "Disabled Button").click   }  
-    
-    $ie.button(:caption, "Click Me").click
-    assert($ie.text.include?("PASS") )
+    browser.button(:caption, "Click Me").click
+    assert(browser.text.include?("PASS") )
   end
   
   def test_Button_Exists
-    assert($ie.button(:caption, "Click Me").exists?)   
-    assert($ie.button(:caption, "Submit").exists?)   
-    assert($ie.button(:name, "b1").exists?)   
-    assert($ie.button(:id, "b2").exists?)   
-    assert($ie.button(:caption, /sub/i).exists?)   
+    assert(browser.button(:caption, "Click Me").exists?)   
+    assert(browser.button(:caption, "Submit").exists?)   
+    assert(browser.button(:name, "b1").exists?)   
+    assert(browser.button(:id, "b2").exists?)   
+    assert(browser.button(:caption, /sub/i).exists?)   
     
-    assert(!$ie.button(:caption, "missingcaption").exists?)   
-    assert(!$ie.button(:name, "missingname").exists?)   
-    assert(!$ie.button(:id, "missingid").exists?)   
-    assert(!$ie.button(:caption, /missing/i).exists?)   
+    assert(!browser.button(:caption, "missingcaption").exists?)   
+    assert(!browser.button(:name, "missingname").exists?)   
+    assert(!browser.button(:id, "missingid").exists?)   
+    assert(!browser.button(:caption, /missing/i).exists?)   
   end
   
   def test_Button_Enabled
     assert($ie.button(:caption, "Click Me").enabled?)   
-    assert(!$ie.button(:caption, "Disabled Button").enabled?)   
-    assert(!$ie.button(:name, "b4").enabled?)   
-    assert(!$ie.button(:id, "b5").enabled?)   
+    assert(!browser.button(:caption, "Disabled Button").enabled?)   
+    assert(!browser.button(:name, "b4").enabled?)   
+    assert(!browser.button(:id, "b5").enabled?)   
     
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   $ie.button(:name, "noName").enabled?  }  
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown" ) {   browser.button(:name, "noName").enabled?  }  
   end
   
   def test_frame
-    goto_frames_page
-    
-    assert($ie.frame("buttonFrame").button(:caption, "Click Me").enabled?)   
-    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown ( no frame name supplied) " ) { $ie.button(:caption, "Disabled Button").enabled?}  
+    use_page "frame_buttons.html"
+    assert(browser.frame("buttonFrame").button(:caption, "Click Me").enabled?)   
+    assert_raises(UnknownObjectException , "UnknownObjectException was supposed to be thrown ( no frame name supplied) " ) { browser.button(:caption, "Disabled Button").enabled?}  
   end
   
 end
