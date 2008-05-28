01f10ce379e572b0d91a9362613c8d64ef4911d7
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-05-28 22:59:38 +0000

    You now get a readable error message when you fail to locate an element using multiple attributes. Also changed the formal slightly of the error message even when only one attribute is used. Fix for http://jira.openqa.org/browse/WTR-171

--- unittests/links_multi_test.rb
+++ unittests/links_multi_test.rb
@@ -7,29 +7,44 @@ require 'unittests/setup'
 class TC_Links_Multi < Test::Unit::TestCase
   
   def setup
-    $ie.goto $htmlRoot + 'links_multi.html'
+    browser.goto $htmlRoot + 'links_multi.html'
   end
   
   def test_existing
-    assert_match(/not.html/, $ie.link(:class_name, 'Papa').href)
-    assert_match(/mama.html/, $ie.link(:text, 'click').href)
+    assert_match(/not.html/, browser.link(:class_name, 'Papa').href)
+    assert_match(/mama.html/, browser.link(:text, 'click').href)
   end
   
   def test_class_alias
-    assert_match(/not.html/, $ie.link(:class, 'Papa').href)
+    assert_match(/not.html/, browser.link(:class, 'Papa').href)
   end
 
   def test_hash_syntax
-    assert_match(/not.html/, $ie.link(:class_name => 'Papa').href)
-    assert_match(/mama.html/, $ie.link(:text => 'click').href)
+    assert_match(/not.html/, browser.link(:class_name => 'Papa').href)
+    assert_match(/mama.html/, browser.link(:text => 'click').href)
   end
   
   def test_class_and_text
-    assert_match(/papa.html/, $ie.link(:class => 'Papa', :text => 'click').href)
+    assert_match(/papa.html/, browser.link(:class => 'Papa', :text => 'click').href)
   end
   
   def test_class_and_index
-    assert_match(/papa.html/, $ie.link(:class => 'Papa', :index => 2).href)
+    assert_match(/papa.html/, browser.link(:class => 'Papa', :index => 2).href)
   end  
+
+  include Watir::Exception
+  def test_not_found_single
+    exception = assert_raise(UnknownObjectException) do
+      browser.link(:id, 'Missing').href
+    end
+    assert_equal('Unable to locate element, using :id, "Missing"', exception.message)
+  end
+  
+  def test_not_found_with_multi
+    exception = assert_raise(UnknownObjectException) do
+      browser.link(:class => 'Missing', :index => 2).href
+    end
+    assert_equal('Unable to locate element, using {:class=>"Missing", :index=>2}', exception.message)
+  end
   
 end
