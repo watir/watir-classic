db33e45a1e0e5162eb2526184ed5ed5298464d3b
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-05-29 03:44:17 +0000

    Name collision removed (duplicate TC_Tables test case classes), thereby fixing one of the test failures in all_tests.rb

--- unittests/nbsp_xpath_test.rb
+++ unittests/nbsp_xpath_test.rb
@@ -4,14 +4,14 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') if $0 == __FILE__
 require 'unittests/setup'
 
-class TC_Tables < Test::Unit::TestCase
+class TC_Nbsp_Xpath < Test::Unit::TestCase
   
   def setup
     use_page "xpath_nbsp.html"
   end
   
   def test_nbsp
-	div = $ie.element_by_xpath("//div")
-	assert(div.innerText, "Hello world")
+  	div = $ie.element_by_xpath("//div")
+  	assert(div.innerText, "Hello world")
   end
 end
