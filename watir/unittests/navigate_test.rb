8d5101c2672c25f01bc75281cb43bbe4a749d092
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-07-08 03:02:33 +0000

    fixed test so that it now passes when running out of a work directory with a space in it. also simplified the test and arguably made it more correct.

--- watir/unittests/navigate_test.rb
+++ watir/unittests/navigate_test.rb
@@ -6,24 +6,19 @@ require 'unittests/setup'
 require 'cgi'
 
 class TC_Navigate < Test::Unit::TestCase
-  
   def test_navigation
     $ie.clear_url_list
     goto_page 'buttons1.html'
-    url = $ie.url.downcase
-    url = CGI::unescape url 
-    assert_equal(($htmlRoot + 'buttons1.html').downcase, url)  # sometimes we get capital drive letters 
+    assert_equal(($htmlRoot + 'buttons1.html').gsub(" ","%20"), $ie.url)  
 
     assert_equal(1, $ie.url_list.length)
-    assert_equal(url, $ie.url_list[0].downcase)
+    assert_equal($ie.url, $ie.url_list[0])
     
     goto_page 'checkboxes1.html'
-    url = $ie.url.downcase   
-    url = CGI::unescape url 
     assert_equal("Test page for Check Boxes", $ie.title) 
 
     assert_equal(2, $ie.url_list.length)
-    assert_equal(url, $ie.url_list[1].downcase)
+    assert_equal($ie.url, $ie.url_list[1])
     
     $ie.clear_url_list
     assert_equal(0, $ie.url_list.length )
