83056f2ccb108857a82cbf120a7d63cb2b435014
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-08-28 01:29:32 +0000

    Converted this test to use browser instead of $ie. This allows it to work with FireWatir. (This is in core tests; i missed previously.)

--- watir/unittests/textfield_for_ch_char_test.rb
+++ watir/unittests/textfield_for_ch_char_test.rb
@@ -8,23 +8,23 @@ class TC_Fields_For_Chinese_Char < Test::Unit::TestCase
   end
   
   def test_chinese_char_should_be_appended_to_text_field
-    $ie.text_field(:name, "text1").append(" ĳĳ")
-    assert_equal(  "Hello World ĳĳ" , $ie.text_field(:name, "text1").getContents )  
+    browser.text_field(:name, "text1").append(" ĳĳ")
+    assert_equal(  "Hello World ĳĳ" , browser.text_field(:name, "text1").getContents )  
   end
   
   def test_mixed_char_should_be_appended_to_text_field
-    $ie.text_field(:name, "text1").append(" ĳaĳa")
-    assert_equal(  "Hello World ĳaĳa" , $ie.text_field(:name, "text1").getContents )  
+    browser.text_field(:name, "text1").append(" ĳaĳa")
+    assert_equal(  "Hello World ĳaĳa" , browser.text_field(:name, "text1").getContents )  
   end
   
   def test_chinese_char_should_be_set_to_text_field
-    $ie.text_field(:name, "text1").set("ĳĳ")
-    assert_equal(  "ĳĳ" , $ie.text_field(:name, "text1").getContents )  
+    browser.text_field(:name, "text1").set("ĳĳ")
+    assert_equal(  "ĳĳ" , browser.text_field(:name, "text1").getContents )  
   end
   
   def test_mixed_char_should_be_set_to_text_field
-    $ie.text_field(:name, "text1").set("ĳaĳa")
-    assert_equal(  "ĳaĳa" , $ie.text_field(:name, "text1").getContents )  
+    browser.text_field(:name, "text1").set("ĳaĳa")
+    assert_equal(  "ĳaĳa" , browser.text_field(:name, "text1").getContents )  
   end
   
 end
