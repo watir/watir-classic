b7c90d0f3a756254f7cc627d0071fa9d7a6c1824
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-09-25 16:28:42 +0000

    Removing examples, as they have moved to the wiki.

--- watir/examples/CookieHelperExample.rb
+++
@@ -1,9 +0,0 @@
-# this should be moved to watir/contrib
-
-require 'watir'
-require 'watir/cookiemanager'
-include Watir
-include CookieManager
-
-puts WatirHelper.getSpecialFolderLocation(WatirHelper::COOKIES)
-WatirHelper.deleteSpecialFolderContents(WatirHelper::COOKIES)
