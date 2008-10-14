42b72fadf3bdafd61d6f54a0d1865ee9b7ce0b62
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-10-14 00:49:18 +0000

    More test cleanup.

--- watir/unittests/form_test.rb
+++ watir/unittests/form_test.rb
@@ -116,29 +116,31 @@ class TC_Forms3 < Test::Unit::TestCase
   end
   
   def test_flash1
-    browser.form(:name ,"test2").button(:caption , "Submit").flash
+    assert_nothing_raised do
+      browser.form(:name, "test2").button(:caption , "Submit").flash
+    end
   end 
   
   def test_objects_with_same_name
-    assert_equal('textfield' ,browser.text_field( :name , 'g1').value )
-    assert_equal('button'    ,browser.button(     :name , 'g1').value )
-    assert_equal('1'         ,browser.checkbox(   :name , 'g1').value )
-    assert_equal('2'         ,browser.radio(      :name , 'g1').value )
+    assert_equal('textfield' ,browser.text_field( :name, 'g1').value )
+    assert_equal('button'    ,browser.button(     :name, 'g1').value )
+    assert_equal('1'         ,browser.checkbox(   :name, 'g1').value )
+    assert_equal('2'         ,browser.radio(      :name, 'g1').value )
     
-    assert_equal('textfield' ,browser.text_field( :name , /g1/).value )
-    assert_equal('button'    ,browser.button(     :name , /g1/).value )
-    assert_equal('1'         ,browser.checkbox(   :name , /g1/).value )
-    assert_equal('2'         ,browser.radio(      :name , /g1/).value )
+    assert_equal('textfield' ,browser.text_field( :name, /g1/).value )
+    assert_equal('button'    ,browser.button(     :name, /g1/).value )
+    assert_equal('1'         ,browser.checkbox(   :name, /g1/).value )
+    assert_equal('2'         ,browser.radio(      :name, /g1/).value )
     
-    assert_equal('textfield_id' ,browser.text_field( :id , 'g1').value )
-    assert_equal('button_id'    ,browser.button(     :id , 'g1').value )
-    assert_equal('1_id'         ,browser.checkbox(   :id , 'g1').value )
-    assert_equal('2_id'         ,browser.radio(      :id , 'g1').value )
+    assert_equal('textfield_id' ,browser.text_field( :id, 'g1').value )
+    assert_equal('button_id'    ,browser.button(     :id, 'g1').value )
+    assert_equal('1_id'         ,browser.checkbox(   :id, 'g1').value )
+    assert_equal('2_id'         ,browser.radio(      :id, 'g1').value )
 
-    assert_equal('textfield_id' ,browser.text_field( :id , /g1/).value )
-    assert_equal('button_id'    ,browser.button(     :id , /g1/).value )
-    assert_equal('1_id'         ,browser.checkbox(   :id , /g1/).value )
-    assert_equal('2_id'         ,browser.radio(      :id , /g1/).value )
+    assert_equal('textfield_id' ,browser.text_field( :id, /g1/).value )
+    assert_equal('button_id'    ,browser.button(     :id, /g1/).value )
+    assert_equal('1_id'         ,browser.checkbox(   :id, /g1/).value )
+    assert_equal('2_id'         ,browser.radio(      :id, /g1/).value )
   end
   
   def test_flash2
@@ -147,17 +149,17 @@ class TC_Forms3 < Test::Unit::TestCase
   end
   
   def test_submitWithImage
-    assert( browser.button(:alt , "submit").exists? )
-    assert( browser.button(:alt , /sub/).exists? )
+    assert( browser.button(:alt, "submit").exists? )
+    assert( browser.button(:alt, /sub/).exists? )
     
-    assert_false( browser.button(:alt , "missing").exists? )
-    assert_false( browser.button(:alt , /missing/).exists? )
+    assert_false( browser.button(:alt, "missing").exists? )
+    assert_false( browser.button(:alt, /missing/).exists? )
     
     #assert( browser.button(:src , "file:///#{$myDir}/html/images/button.jpg").exists? )    # this doesnt work for everybody
-    assert( browser.button(:src , /button/).exists? )
+    assert( browser.button(:src, /button/).exists? )
     
-    assert_false( browser.button(:src , "missing").exists? )
-    assert_false( browser.button(:src , /missing/).exists? )
+    assert_false( browser.button(:src, "missing").exists? )
+    assert_false( browser.button(:src, /missing/).exists? )
     assert_nothing_raised("raised an exception when it shouldnt have") { browser.button(:src , /button/).click }
     
     assert( browser.text.include?("PASS") )
@@ -182,7 +184,7 @@ class TC_Forms4 < Test::Unit::TestCase
   end
   
   def test_find_text_field_ignoring_form
-    assert_equal(browser.text_field(:name, 'name').value, 'apple') # should it raise a not-unique error instead?
+    assert_equal(browser.text_field(:name, 'name').value, 'apple')
   end
   
   def test_correct_form_field_is_found_using_form_name
@@ -202,7 +204,7 @@ class TC_Forms4 < Test::Unit::TestCase
   
   def test_submit
     browser.form(:name, 'apple_form').submit
-    assert( browser.text.include?("PASS") )
+    assert(browser.text.include?("PASS"))
   end
 end
 
@@ -214,31 +216,31 @@ class TC_Hidden_Fields < Test::Unit::TestCase
   def test_hidden
     
     # test using index
-    assert( browser.hidden(:index,1).exists? )
-    assert( browser.hidden(:index,2).exists? )
-    assert_false( browser.hidden(:index,3).exists? )
+    assert( browser.hidden(:index, 1).exists? )
+    assert( browser.hidden(:index, 2).exists? )
+    assert_false( browser.hidden(:index, 3).exists? )
     
-    browser.hidden(:index,1).value = 44
-    browser.hidden(:index,2).value = 55
-    
-    browser.button(:value , "Show Hidden").click
-    
-    assert_equal("44"  , browser.text_field(:name , "vis1").value ) 
-    assert_equal("55"  , browser.text_field(:name , "vis2").value )
-    
-    # test using name and ID
-    assert( browser.hidden(:name ,"hid1").exists? )
-    assert( browser.hidden(:id,"hidden_1").exists? )
-    assert_false( browser.hidden(:name,"hidden_44").exists? )
-    assert_false( browser.hidden(:id,"hidden_55").exists? )
-    
-    browser.hidden(:name ,"hid1").value = 444
-    browser.hidden(:id,"hidden_1").value = 555
+    browser.hidden(:index, 1).value = 44
+    browser.hidden(:index, 2).value = 55
     
     browser.button(:value, "Show Hidden").click
     
-    assert_equal("444", browser.text_field(:name , "vis1").value ) 
-    assert_equal("555", browser.text_field(:name ,"vis2").value )
+    assert_equal("44", browser.text_field(:name, "vis1").value ) 
+    assert_equal("55", browser.text_field(:name, "vis2").value )
+    
+    # test using name and ID
+    assert( browser.hidden(:name, "hid1").exists? )
+    assert( browser.hidden(:id, "hidden_1").exists? )
+    assert_false( browser.hidden(:name, "hidden_44").exists? )
+    assert_false( browser.hidden(:id, "hidden_55").exists? )
+    
+    browser.hidden(:name, "hid1").value = 444
+    browser.hidden(:id, "hidden_1").value = 555
+    
+    browser.button(:value, "Show Hidden").click
+    
+    assert_equal("444", browser.text_field(:name, "vis1").value) 
+    assert_equal("555", browser.text_field(:name, "vis2").value)
     
     # test the over-ridden append method
     browser.hidden(:name, "hid1").append("a")
@@ -253,22 +255,22 @@ class TC_Hidden_Fields < Test::Unit::TestCase
     assert_equal("555", browser.text_field(:name, "vis2").value)
     
     # test using a form
-    assert( browser.form(:name, "has_a_hidden").hidden(:name ,"hid1").exists? )
-    assert( browser.form(:name, "has_a_hidden").hidden(:id,"hidden_1").exists? )
-    assert_false( browser.form(:name, "has_a_hidden").hidden(:name,"hidden_44").exists? )
-    assert_false( browser.form(:name, "has_a_hidden").hidden(:id,"hidden_55").exists? )
+    assert( browser.form(:name, "has_a_hidden").hidden(:name, "hid1").exists? )
+    assert( browser.form(:name, "has_a_hidden").hidden(:id, "hidden_1").exists? )
+    assert_false( browser.form(:name, "has_a_hidden").hidden(:name, "hidden_44").exists? )
+    assert_false( browser.form(:name, "has_a_hidden").hidden(:id, "hidden_55").exists? )
     
-    browser.form(:name, "has_a_hidden").hidden(:name ,"hid1").value = 222
-    browser.form(:name, "has_a_hidden").hidden(:id,"hidden_1").value = 333
+    browser.form(:name, "has_a_hidden").hidden(:name, "hid1").value = 222
+    browser.form(:name, "has_a_hidden").hidden(:id, "hidden_1").value = 333
     
     browser.button(:value, "Show Hidden").click
     
-    assert_equal("222", browser.text_field(:name , "vis1").value) 
-    assert_equal("333", browser.text_field(:name ,"vis2").value)
+    assert_equal("222", browser.text_field(:name, "vis1").value) 
+    assert_equal("333", browser.text_field(:name, "vis2").value)
     
     # iterators
     assert_equal(2, browser.hiddens.length)
-    count =1
+    count = 1
     browser.hiddens.each do |h|
       case count
       when 1
@@ -278,7 +280,7 @@ class TC_Hidden_Fields < Test::Unit::TestCase
         assert_equal("", h.name)
         assert_equal("hidden_1", h.id)
       end
-      count+=1
+      count += 1
     end
     
     assert_equal("hid1", browser.hiddens[1].name)
