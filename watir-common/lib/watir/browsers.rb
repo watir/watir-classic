04e8f789242c32378ad9c41bc86c04132b841cfc
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-10-30 22:31:04 +0000

    Document Browser class. Add support (untested) for Browser to support Safari.

--- watir-common/lib/watir/browsers.rb
+++ watir-common/lib/watir/browsers.rb
@@ -7,4 +7,7 @@ Watir::Browser.support 'ie', 'Watir::IE', [:speed, :visible]
 module FireWatir; autoload :Firefox, 'firewatir'; end
 Watir::Browser.support 'firefox', 'FireWatir::Firefox'
 
+module Watir; autoload :Safari, 'safariwatir'; end
+Watir::Browser.support 'safari', 'Watir::Safari'
+
 
