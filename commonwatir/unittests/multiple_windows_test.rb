f2d1c3572a8dbbdb6a82dbb065ef092ef94f6c26
bret <bret@deaf61b0-630d-0410-ad59-fc55e6353476>
2008-12-04 00:05:09 +0000

    Remove empty test committed in error (which broke the build).

--- commonwatir/unittests/multiple_windows_test.rb
+++
@@ -1,9 +0,0 @@
-# feature tests for title and url
-
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') unless $SETUP_LOADED
-require 'unittests/setup'
-
-class TC_MultiplePages < Test::Unit::TestCase
-  location __FILE__
-
-end
