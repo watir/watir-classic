261bc2c519640a601c932482aa115ef59fdb2955
Zeljko Filipin <zeljko.filipin@gmail.com>
2009-05-22 17:08:19 +0200

    removed debugging code

--- watir/lib/watir/input_elements.rb
+++ watir/lib/watir/input_elements.rb
@@ -444,7 +444,7 @@ module Watir
       begin
         thrd = Thread.new do
           file_field_set =
-            "ruby -e
+            "rubyw -e
               \"require 'win32ole'
               @autoit = WIN32OLE.new('AutoItX3.Control')
               # inspect inserts array in this string, instead of string
@@ -457,7 +457,6 @@ module Watir
                   @autoit.ControlSend(popup_title, '', 'Button2', '{ENTER}')
                 end
               end\""
-          puts file_field_set
           system file_field_set
         end
       thrd.join(1)
