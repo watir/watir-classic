2d166917b925e1ea571e0007091facb46630500b
Zeljko Filipin <zeljko.filipin@gmail.com>
2009-05-22 15:55:18 +0200

    separaded each ruby command to own line

--- watir/lib/watir/input_elements.rb
+++ watir/lib/watir/input_elements.rb
@@ -445,9 +445,17 @@ module Watir
           popup_title = 'Choose file'
           file_field_set =
             "rubyw
-            -e \"require 'win32ole'; @autoit=WIN32OLE.new('AutoItX3.Control'); waitresult=@autoit.WinWait '#{popup_title}', '', 15; sleep 1; if waitresult == 1\"
-            -e \"@autoit.ControlSetText '#{popup_title}', '', 'Edit1', '#{setPath}'; @autoit.ControlSend '#{popup_title}', '', 'Button2', '{ENTER}';\"
-            -e \"end\""
+            -e
+              \"require 'win32ole';
+              @autoit=WIN32OLE.new('AutoItX3.Control');
+              waitresult=@autoit.WinWait '#{popup_title}', '', 15;
+              sleep 1;
+              if waitresult == 1\"
+            -e
+              \"@autoit.ControlSetText '#{popup_title}', '', 'Edit1', '#{setPath}';
+              @autoit.ControlSend '#{popup_title}', '', 'Button2', '{ENTER}';\"
+            -e
+              \"end\""
           system file_field_set
         end
       thrd.join(1)
