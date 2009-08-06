e51c0ba3b5bc1a5395ea9f7acaf53e9d916ac2f1
jarib <jari.bakken@gmail.com>
2009-08-06 02:30:51 +0200

    FireWatir 1.9-fix: Inspect string should allow negative hash values

--- commonwatir/unittests/inspect_test.rb
+++ commonwatir/unittests/inspect_test.rb
@@ -9,20 +9,20 @@ class TC_Inspect < Test::Unit::TestCase
   end
 
   def test_inspect_only_returns_url_and_title
-    assert_match(%r{#<#{browser.class}:0x[0-9a-f]+ url="file://.+/emphasis\.html" title="emphasis">}, browser.inspect)
+    assert_match(%r{#<#{browser.class}:0x[0-9a-f.]+ url="file://.+/emphasis\.html" title="emphasis">}, browser.inspect)
   end
 
   def test_element_inspect
-    assert_match(%r{^#<.+::Em:0x[0-9a-f]+ located=false how=:id what=/em-one/>$}, browser.em(:id, /em-one/).inspect)
+    assert_match(%r{^#<.+::Em:0x[0-9a-f.]+ located=false how=:id what=/em-one/>$}, browser.em(:id, /em-one/).inspect)
 
     located = browser.em(:id, "em-one")
     located.exists?
 
-    assert_match(%r{^#<.+::Em:0x[0-9a-f]+ located=true how=:id what="em-one">$}, located.inspect)
+    assert_match(%r{^#<.+::Em:0x[0-9a-f.]+ located=true how=:id what="em-one">$}, located.inspect)
   end
 
   def test_element_collections_inspect
-    assert_match(%r{#<.+::Ems:0x[0-9a-f]+ length=3 container=.+>$}, browser.ems.inspect)
+    assert_match(%r{#<.+::Ems:0x[0-9a-f.]+ length=3 container=.+>$}, browser.ems.inspect)
   end
 
 end
