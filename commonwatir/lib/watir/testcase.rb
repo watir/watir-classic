dc6db05dca06f8d262e5bf37e3f5621e01842633
marekj <marekj.com@gmail.com>
2010-02-04 06:40:57 +0800

    test case example with browser

--- commonwatir/lib/watir/testcase.rb
+++ commonwatir/lib/watir/testcase.rb
@@ -3,6 +3,45 @@ require 'watir/assertions'
 
 module Watir
 
+  # This is a 'test/unit' testcase customized to exeucte test methods sequentially by default
+  # and extra assertions
+  #
+  # Example Usage
+  #
+  #   require 'watir/testcase'
+  #
+  #   class MyTestCase < Watir::TestCase
+  #
+  #     # some helpers
+  #     @@browser = nil
+  #     def browser
+  #       @browser ||= Watir::IE.start(:url, 'http://watir.com/")
+  #     end
+  #
+  #     # TESTS
+  #     def test_text
+  #       browser.goto "http://watir.com/"
+  #       verify_match "Web Application Testing in Ruby", browser.text
+  #     end
+  #
+  #     def test_title
+  #       verify browser.title == 'Watir'
+  #     end
+  #
+  #     def test_link
+  #       verify_match 'watir.com', browser.link(:text, 'Home').href
+  #     end
+  #
+  #     def test_navigate_to_examples
+  #       browser.div(:id, 'nav').link(:text, 'Examples').click
+  #     end
+  #
+  #     def test_url
+  #       verify_equal browser.url, 'http://watir.com/examples/'
+  #     end
+  #
+  #   end
+  #
   class TestCase < Test::Unit::TestCase
     include Watir::Assertions
     @@order = :sequentially
