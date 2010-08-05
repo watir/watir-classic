1ae75895b95534632a41bbc2c37da394b7057203
Jari Bakken <jari.bakken@gmail.com>
2010-08-05 14:43:24 +0200

    Bump watirspec to 521f05e, update spec_helper.rb for revamped guard system.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,19 @@ require "watir"
 case ENV['watir_browser']
 when /firefox/
   Browser = FireWatir::Firefox
+  browser = :firefox
 else
   Browser = Watir::IE
   WatirSpec.persistent_browser = true
+  browser = :ie
 end
+
+WatirSpec.implementation do |imp|
+  imp.name = :watir
+  imp.guard_proc = lambda { |args|
+    args.any? { |arg| arg == :watir || arg == [:watir, browser] }
+  }
+end
+
 
 include Watir::Exception
