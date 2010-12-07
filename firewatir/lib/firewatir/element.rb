b56664d35cab6cfbdbc607145a0d88b7982a4ca6
Alok Menghrajani <alok@facebook.com>
2010-12-07 09:31:16 -0800

    Fix exists? which was incorrectly returning true in some cases
    We have a bug in the way elements are located. Given the following page:
    <html>
      <body>
        <div id="foo">
          <div id="bar">
          </div>
        </div>
      </body>
    </html>
    
    And the following test cases:
    1. Browser.div(:id, 'foo').exists? => true
    2. Browser.div(:id, 'bar').exists? => true
    3. Browser.div(:id, 'foo').div(:id, 'bar').exists? => true
    4. Browser.div(:id, 'bar').div(:id, 'foo').exists? => false
    5. Browser.div(:id, 'something').div(:id, 'else').exists? => true
    6. Browser.div(:id, 'foo').div(:id, 'hello').exists? => false
    
    Case 5 incorrectly returns true. This diff fixes this bug.
    
    Note:
    there might be a more elegant way of fixing things, but I'm not super familiar with the jssh syntax. Feel
    free to suggest a better fix.

--- firewatir/lib/firewatir/element.rb
+++ firewatir/lib/firewatir/element.rb
@@ -281,6 +281,13 @@ module FireWatir
         #puts "container name is: " + @container.element_name
         #locate if defined? locate
         #@container.locate
+	
+	# We cannot assume that the container exists at this point, because code like:
+	# b.div(:id, "something_that_does_not_exist").h2(:text, /foobar/).exists? would return true
+	if (!@container.exists?)
+	  return nil
+	end
+
         jssh_command << "var elements_#{@@current_level}_#{tag} = #{@container.element_name}.getElementsByTagName(\"#{tag}\");"
         if(types != nil and (types.include?("textarea") or types.include?("button") ) )
           jssh_command << "elements_#{@@current_level}_#{tag} = #{@container.element_name}.getElementsByTagName(\"*\");"
