8963236f62dd97b2158ecae535ed0e256e5f3909
Hugh McGowan <hugh_mcgowan@yahoo.com>
2011-08-31 10:55:39 -0500

    fix handling of document mode when called from a frame

--- watir/unittests/document_standards.rb
+++ watir/unittests/document_standards.rb
@@ -2,6 +2,22 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') unless $SETUP_LOADED
 require 'unittests/setup'
 
+class TC_DocumentStandards_Quirks < Test::Unit::TestCase
+  include Watir::Exception
+  
+  def setup
+    goto_page 'quirks_document_standards.html'
+  end
+
+  def test_elements_exist_or_not
+    assert(browser.document_mode.to_i == 5)
+    assert(browser.text_field(:name,"quirks_text").exists?)
+    browser.text_field(:name,"quirks_text").set "test"
+    browser.text_field(:name,"quirks_text").fire_event "onClick"
+    assert(browser.text_field(:name,"quirks_text").value == 'test')
+  end
+end
+
 class TC_DocumentStandards_IE7 < Test::Unit::TestCase
   include Watir::Exception
   
