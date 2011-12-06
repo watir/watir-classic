f09496779c1f03780e2389f2497a314246aed90e
Jarmo Pertman <jarmo.p@gmail.com>
2011-12-06 23:14:01 +0200

    better solution for prepending http:// in Browser#goto

--- watir/lib/watir/ie-class.rb
+++ watir/lib/watir/ie-class.rb
@@ -387,7 +387,7 @@ module Watir
     # Navigate to the specified URL.
     #  * url - string - the URL to navigate to
     def goto(url)
-      url = "http://" + url unless url =~ %r{://} || url == "about:blank"
+      url = "http://" + url unless url =~ URI.regexp
       @ie.navigate(url)
       wait
       return @down_load_time
