56c6e196f4d8ffdd887234ac5a73d6d60fcc447f
Jarmo Pertman <jarmo.p@gmail.com>
2012-01-09 12:48:17 +0200

    require rubygems just in case for Image#save

--- watir/lib/watir/image.rb
+++ watir/lib/watir/image.rb
@@ -111,7 +111,7 @@ module Watir
     end
     
     def fill_save_image_dialog(path)
-      command = "require 'rautomation';" <<
+      command = "require 'rubygems';require 'rautomation';" <<
                 "window=::RAutomation::Window.new(:title => 'Save Picture');" <<
                 "window.text_field(:class => 'Edit', :index => 0).set('#{path.gsub(File::SEPARATOR, File::ALT_SEPARATOR)}');" <<
                 "window.button(:value => '&Save').click"
