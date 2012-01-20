cf5e3203ee0f77de70eeb496adba29e683247af7
Hugh McGowan <hugh_mcgowan@yahoo.com>
2012-01-20 14:20:18 -0600

    for IE9 onward, change -nomerge startup option to -noframemerging. The -nomerge option was deprecated.

--- watir/lib/watir/ie-process.rb
+++ watir/lib/watir/ie-process.rb
@@ -6,7 +6,8 @@ module Watir
       def self.start
         program_files = ENV['ProgramFiles'] || "c:\\Program Files"
         startup_command = "#{program_files}\\Internet Explorer\\iexplore.exe"
-        startup_command << " -nomerge" if IE.version_parts.first.to_i >= 8
+        startup_command << " -nomerge" if IE.version_parts.first.to_i == 8
+        startup_command << " -noframemerging" if IE.version_parts.first.to_i >= 9
         process_info = ::Process.create('app_name' => "#{startup_command} about:blank")
         process_id = process_info.process_id
         new process_id
