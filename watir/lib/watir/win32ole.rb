fba1b6b5fae8b4fc60d37463131dbeb8bd706e2b
Hugh McGowan <hugh_mcgowan@yahoo.com>
2012-02-06 12:12:45 -0600

    add support for modal dialogs in Ruby 1.9.3

--- watir/lib/watir/win32ole.rb
+++ watir/lib/watir/win32ole.rb
@@ -4,9 +4,8 @@
 
 if RUBY_VERSION =~ /^1\.8/
   $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.8.7'))
-# WIP
-#elsif RUBY_VERSION =~ /^1\.9/
-#  $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.2'))
+elsif RUBY_VERSION =~ /^1\.9/
+  $LOAD_PATH.unshift  File.expand_path(File.join(File.dirname(__FILE__), '..', 'watir', 'win32ole', '1.9.3'))
 else
   # loading win32ole from stdlib
 end
