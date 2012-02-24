5385b074f82ad4143e4e9af6698ba046316162d3
Jarmo Pertman <jarmo.p@gmail.com>
2012-02-24 14:56:24 +0200

    rename ElementCollections to ElementCollection

--- watir/lib/watir/collections.rb
+++ watir/lib/watir/collections.rb
@@ -1,11 +1,11 @@
 module Watir
-  class InputElementCollections < ElementCollections
+  class InputElementCollection < ElementCollection
     def each
       @container.locator_for(InputElementLocator, element_class::INPUT_TYPES, @how, @what, element_class).each {|element| yield element}
     end    
   end
 
-  class HTMLElements < ElementCollections
+  class HTMLElements < ElementCollection
     def each
       @container.locator_for(TaggedElementLocator, ["*"], @how, @what, Element).each { |element| yield element }
     end
@@ -13,27 +13,27 @@ module Watir
 
   # this class accesses the check boxes in the document as a collection
   # Normally a user would not need to create this object as it is returned by the Watir::Container#checkboxes method
-  class CheckBoxes < InputElementCollections
+  class CheckBoxes < InputElementCollection
     def element_class; CheckBox; end
   end
     
   # this class accesses the select boxes in the document as a collection
   # Normally a user would not need to create this object as it is returned by the Watir::Container#select_lists method
-  class SelectLists < InputElementCollections
+  class SelectLists < InputElementCollection
     def element_tag; 'SELECT'; end
   end  
 
   %w[Button FileField Radio TextField TextArea Hidden].each do |element|
     module_eval %Q{
-      class #{element}s < InputElementCollections; end
+      class #{element}s < InputElementCollection; end
     }
   end
 
-  class Inses < ElementCollections
+  class Inses < ElementCollection
     def element_class; Ins; end
   end
 
-  class TableSectionCollection < ElementCollections
+  class TableSectionCollection < ElementCollection
     def element_class; TableSection; end
   end
 
@@ -41,7 +41,7 @@ module Watir
      Label Pre P Span Div Dl Dt Dd Strong Em Del
      Font H1 H2 H3 H4 H5 H6 Meta Ol Ul FieldSet Option].each do |element|
     module_eval %Q{
-      class #{element}s < ElementCollections; end
+      class #{element}s < ElementCollection; end
     }
   end
   
--- watir/lib/watir/element_collection.rb
+++ watir/lib/watir/element_collection.rb
@@ -1,7 +1,7 @@
 module Watir
   # this class is the super class for the iterator classes (buttons, links, spans etc
   # it would normally only be accessed by the iterator methods (spans, links etc) of IE
-  class ElementCollections
+  class ElementCollection
     include Enumerable
 
     # Super class for all the iterator classes
