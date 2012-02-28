b22711cafc89b43bf61fd26af29c9908355433d1
Jarmo Pertman <jarmo.p@gmail.com>
2012-02-28 23:45:39 +0200

    raise ArgumentError if more specifiers are used with :xpath

--- watir/lib/watir/supported_elements.rb
+++ watir/lib/watir/supported_elements.rb
@@ -34,7 +34,7 @@ module Watir
     def format_specifiers(tag_name, how, what)
       defaults = {:tag_name => [tag_name].flatten.map(&:to_s)}
       formatted_specifiers = defaults.merge(what ? {how => what} : how)
-      if formatted_specifiers[:xpath] && formatted_specifiers.size > 1
+      if formatted_specifiers[:xpath] && formatted_specifiers.size > 2
         raise ArgumentError, ":xpath specifier should be the only one when used in #{formatted_specifiers.inspect}"
       end
       formatted_specifiers
