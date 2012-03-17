dbe51436fb873677305ab60f9e74044aaa0a5159
Jarmo Pertman <jarmo.p@gmail.com>
2012-03-18 00:13:55 +0200

    use center of elements when drag and dropping

--- watir/lib/watir/drag_and_drop_helper.rb
+++ watir/lib/watir/drag_and_drop_helper.rb
@@ -4,8 +4,7 @@ module Watir
     def drag_and_drop_on(target)
       perform_action do
         assert_target target
-        drop_x = target.send :left_edge_absolute
-        drop_y = target.send :top_edge_absolute
+        drop_x, drop_y = target.send :source_x_y
         drag_to drop_x, drop_y
       end
     end
@@ -22,14 +21,14 @@ module Watir
     def drag_to(drop_x, drop_y)
       drag_x, drag_y = source_x_y
       mouse = page_container.rautomation.mouse
-      mouse.move :x => drag_x + 10, :y => drag_y + 10
+      mouse.move :x => drag_x , :y => drag_y
       mouse.press
-      mouse.move :x => drop_x + 10, :y => drop_y + 10
+      mouse.move :x => drop_x, :y => drop_y
       mouse.release
     end
 
     def source_x_y
-      return left_edge_absolute, top_edge_absolute
+      center_x_y_absolute left_edge_absolute, top_edge_absolute
     end
 
     def assert_target(target)
@@ -51,6 +50,18 @@ module Watir
 
     def left_edge_absolute
       left_edge + page_container.document.parentWindow.screenLeft.to_i
+    end
+
+    def right_edge
+      ole_object.getBoundingClientRect.right.to_i
+    end
+
+    def bottom_edge
+      ole_object.getBoundingClientRect.bottom.to_i
+    end
+
+    def center_x_y_absolute x, y
+      return (right_edge - left_edge) / 2 + x, (bottom_edge - top_edge) / 2 + y
     end
 
   end
