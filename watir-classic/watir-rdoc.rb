03af8e9e9b67e2085d1edc5304d63c7d93eab8fc
Jarmo Pertman <jarmo.p@gmail.com>
2012-07-28 11:10:56 +0300

    Remove attach, watir-console and watir-rdoc files.

--- watir-classic/watir-rdoc.rb
+++
@@ -1,7 +0,0 @@
-$WATIR_RDOC_OPTIONS = [] << 
-      '--title' << 'Watir API Reference' <<
-  		'--accessor' << 'def_wrap=R,def_wrap_guard=R' <<
-  		'--main' << 'README.rdoc' << 
-  		'--exclude' << 'unittests|camel_case.rb'
-$WATIR_EXTRA_RDOC_FILES = ['README.rdoc']
-
