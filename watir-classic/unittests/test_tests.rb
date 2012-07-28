3e3446bdab69092da24da85b7cfae2f458dad52c
Jarmo Pertman <jarmo.p@gmail.com>
2012-07-28 11:09:55 +0300

    Remove old unit tests.

--- watir-classic/unittests/test_tests.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
-
-require 'unittests/setup'
-require 'unittests/checkbox_test'
-
-Watir::UnitTest.filter = proc do |test|
-  !test.tagged?(:firewatir_bug)
-end
-
