50aeb1542f19af53263000c6e6ed984974a17b18
Jarmo Pertman <jarmo.p@gmail.com>
2012-08-03 16:19:45 +0300

    Element#drag_and_drop_on works also with elements not in the viewport.

--- lib/watir-classic/drag_and_drop_helper.rb
+++ lib/watir-classic/drag_and_drop_helper.rb
@@ -4,31 +4,43 @@ module Watir
     def drag_and_drop_on(target)
       perform_action do
         assert_target target
-        drop_x, drop_y = target.send :source_x_y
-        drag_to drop_x, drop_y
+        drag_to do |mouse|
+          ole_object.document.parentWindow.scrollTo(*target.send(:source_x_y_relative))
+          drop_x, drop_y = target.send(:source_x_y)
+          mouse.move :x => drop_x, :y => drop_y
+        end
       end
     end
 
     def drag_and_drop_by(distance_x, distance_y)
       perform_action do
-        drag_x, drag_y = source_x_y
-        drag_to drag_x + distance_x, drag_y + distance_y
+        drag_to do |mouse|
+          drag_x, drag_y = source_x_y
+          ole_object.document.parentWindow.scrollTo(drag_x + distance_x, drag_y + distance_y)
+          drag_x, drag_y = source_x_y
+          mouse.move :x => drag_x + distance_x, :y => drag_y + distance_y
+        end
       end
     end
 
     private
 
-    def drag_to(drop_x, drop_y)
+    def drag_to
+      ole_object.document.parentWindow.scrollTo(*source_x_y_relative)
       drag_x, drag_y = source_x_y
       mouse = page_container.rautomation.mouse
       mouse.move :x => drag_x , :y => drag_y
       mouse.press
-      mouse.move :x => drop_x, :y => drop_y
+      yield mouse
       mouse.release
     end
 
     def source_x_y
       center_x_y_absolute left_edge_absolute, top_edge_absolute
+    end
+
+    def source_x_y_relative
+      center_x_y_absolute left_edge, top_edge
     end
 
     def assert_target(target)
