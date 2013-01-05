43da3606ce72d342fcc552bb7b4956055d78456a
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-06 00:06:33 +0200

    Remove pointless comment.

--- lib/watir-classic.rb
+++ lib/watir-classic.rb
@@ -1,8 +1,3 @@
-#--
-#  The 'watir-classic' library loads the common watir-classic code, common to all watir-classic
-# implementations. The 'watir-classic/browser' library will autoload the actual
-# implementations.
-
 require 'watir-classic/version'
 require 'watir-classic/wait'
 require 'watir-classic/wait_helper'
