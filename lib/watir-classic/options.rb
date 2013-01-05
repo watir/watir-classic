707c8c806b4d7c7a7661fd92a09d54064b207b4e
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-06 01:57:58 +0200

    Do not use class variables. Ever.

--- lib/watir-classic/options.rb
+++ lib/watir-classic/options.rb
@@ -5,30 +5,24 @@ require 'rubygems'
 require 'user-choices'
 
 module Watir
-  @@options_file = nil
-  @@options = nil
   class << self
     # Specify the location of a yaml file containing Watir options. Must be
     # specified before the options are parsed.
-    def options_file= file
-      @@options_file = file
-    end
-    def options_file
-      @@options_file
-    end
-    def options= x
-      @@options = x
-    end
+    attr_accessor :options_file
+
+    attr_writer :options
+
     # Return the Watir options, as a hash. If they haven't been parsed yet,
     # they will be now.
     def options
-      @@options ||= Watir::WatirOptions.new.execute
+      @options ||= Watir::WatirOptions.new.execute
     end
   end
 
   # @private
   class WatirOptions < UserChoices::Command
     include UserChoices
+
     def add_sources builder
       builder.add_source EnvironmentSource, :with_prefix, 'watir_'
       if Watir.options_file
@@ -36,6 +30,7 @@ module Watir
           Watir.options_file
       end
     end
+
     def add_choices builder
       builder.add_choice :browser, 
       :type => Watir::Browser.browser_names, 
@@ -46,9 +41,11 @@ module Watir
       builder.add_choice :visible,
       :type => :boolean
     end
+
     def execute 
       @user_choices[:speed] = @user_choices[:speed].to_sym
       @user_choices
     end 
+
   end
 end
